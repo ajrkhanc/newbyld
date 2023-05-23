74594cbcff3b972286e077b1ba82f7a3ffa87814
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-05-23 10:04:23 +0530

    ch

--- pages/everythingdisc/everything-disc-ttt-ofline-lp.js
+++ pages/everythingdisc/everything-disc-ttt-ofline-lp.js
@@ -21,7 +21,7 @@ export default function DiskTTTLP() {
                     document.getElementById("showlabel").style.display = "block";
 
                     window.setTimeout(function () {
-                        // window.location.href = "/everythingdisc/thank-you"
+                        window.location.href = "/everythingdisc/thank-you"
                     }, 3000);
 
                 } else {
