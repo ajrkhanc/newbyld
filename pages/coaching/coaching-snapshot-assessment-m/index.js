cc52e52e2494b7f2096e6c78dfd99b1070f377fe
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-06-02 12:37:52 +0530

    cv

--- pages/coaching/coaching-snapshot-assessment-m/index.js
+++ pages/coaching/coaching-snapshot-assessment-m/index.js
@@ -52,7 +52,7 @@ export default function BrowseCourses() {
 
 
 
-        var xhr = new XMLHttpRequest();
+        var xhr = new XMLHttpRequest(); 
         xhr.open('POST', 'https://clblogs.vercel.app/api/career-coaching-snapshot');
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.send('trust1=' + trust1 +
