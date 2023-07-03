4216ca4ec50c13f8b5f9c112bbc0ceffb1fa336b
ajrkhanc <ajr.rgs@gmail.com>
2023-07-03 12:46:13 +0530

    jj
    jj

--- pages/_app.js
+++ pages/_app.js
@@ -45,7 +45,7 @@ export default function MyApp({ Component, pageProps, router }) {
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-          <link rel="stylesheet" href="/classets/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+          <link rel="stylesheet" type="text/css" href="/classets/css/bootstrap.min.css"/>
           <script src="https://unpkg.com/phosphor-icons"></script>
           <link rel="stylesheet" href="/classets/css/style.css" />
           <link rel="shortcut icon" href="/classets/favicon.png" type="image/x-icon" />
@@ -115,7 +115,6 @@ export default function MyApp({ Component, pageProps, router }) {
         <Component {...pageProps} />
       </CL>
     )
-
   }
 
 
