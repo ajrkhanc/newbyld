d867161912af408a6a915603b1612700351018b4
ajrkhan1 <102628339+ajrkhanc@users.noreply.github.com>
2023-08-03 16:11:42 +0530

    ds
    sd

--- pages/_app.js
+++ pages/_app.js
@@ -2208,6 +2208,7 @@ gtag('config', 'G-5PXTTR960E');
           <link rel="stylesheet" href="/kbassets/css/style.css" />
           <link rel="stylesheet" href="/kbassets/css/responsive.css" />
           <link rel="stylesheet" href="/kbassets/css/globals.css" />
+          <link rel="stylesheet" href="/kbassets/css/kbnew.css" />
           <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
 
