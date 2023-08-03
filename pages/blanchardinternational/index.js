610f522cc7e2a51974e458a373d9ef0c0bb8c7ef
ajrkhan1 <102628339+ajrkhanc@users.noreply.github.com>
2023-08-03 15:53:32 +0530

    mm
    ll

--- pages/blanchardinternational/index.js
+++ pages/blanchardinternational/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 import Slider from "react-slick";
 
 export default function Home() {
