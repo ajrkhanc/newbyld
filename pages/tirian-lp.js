e05f82c71354303bfc1839fc4ef2c87fd0889958
ajrkhan1 <102628339+ajrkhanc@users.noreply.github.com>
2023-08-14 15:44:22 +0530

    hh
    kk

--- pages/tirian-lp.js
+++ pages/tirian-lp.js
@@ -96,9 +96,8 @@ export default function TirianIndex(){
                                         </div>
                                         <div className="col-sm-6 mb-12">
                                             <select name="slot" required>
-                                                <option value="">Pick up any Slot*</option>
-                                                <option value="Webinar 11th August 2023">Webinar 11th August 2023</option>
-                                                <option value="Webinar 15th September 2023">Webinar 15th September 2023</option>
+                                                <option value="">Pick up any Slot*</option>                                                
+                                                <option value="Showcase 15th September 2023">Showcase 15th September 2023</option>
                                                 <option value="Webinar 6th October 2023">Webinar 6th October 2023</option>
                                                 <option value="Webinar 10th November 2023">Webinar 10th November 2023</option>
                                                 <option value="Webinar 1st December 2023">Webinar 1st December 2023</option>
@@ -163,7 +162,7 @@ export default function TirianIndex(){
                     </div> */}
 
                    
-                    <div className='col-sm-4'>
+                    {/* <div className='col-sm-4'>
                         <div className='eventsbox'>
                             <div className='eventmiddle'>
                                 <img src="/assets/img/events.jpg" alt="" />
@@ -174,13 +173,13 @@ export default function TirianIndex(){
                                 <a className="eventbtn" href='#Reserve'>REGISTER NOW</a>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
                     <div className='col-sm-4'>
                         <div className='eventsbox'>
                             <div className='eventmiddle'>
                                 <img src="/assets/img/events.jpg" alt="" />
-                                <span>Webinar 15th September 2023</span>
+                                <span>Showcase 15th September 2023</span>
                             </div>
                             <div className='eventbottom'>
                                 <h3>Time: 11:00 AM -12:00 PM</h3>
