b14075a2f1d9cf0e7a75661d65465ee14810e92f
ajrkhan1 <102628339+ajrkhanc@users.noreply.github.com>
2023-09-14 17:15:11 +0530

    das
    sd

--- next.config.js
+++ next.config.js
@@ -359,11 +359,11 @@ module.exports = {
       },
       
 
-      {
-        source: '/:path*',
-        destination: 'https://blog.byldgroup.com',
-        permanent: true,
-      },
+      // {
+      //   source: '/:path*',
+      //   destination: 'https://blog.byldgroup.com',
+      //   permanent: true,
+      // },
 
       
 
