1a3bde20bfbff01d0d0da71c600d7b6954a15df5
ajrkhan1 <102628339+ajrkhanc@users.noreply.github.com>
2023-09-14 17:21:10 +0530

    jjf
    jjf

--- next.config.js
+++ next.config.js
@@ -359,11 +359,11 @@ module.exports = {
       },
       
 
-      // {
-      //   source: '/:path*',
-      //   destination: 'https://blog.byldgroup.com',
-      //   permanent: true,
-      // },
+      {
+        source: '/[slug]',
+        destination: 'https://blog.byldgroup.com',
+        permanent: true,
+      },
 
       
 
