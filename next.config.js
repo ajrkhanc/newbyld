88515b55595be44a0cf079794ca9240189031ecf
ajrkhan1 <102628339+ajrkhanc@users.noreply.github.com>
2023-09-14 17:36:35 +0530

    hh
    kk

--- next.config.js
+++ next.config.js
@@ -360,7 +360,7 @@ module.exports = {
       
 
       {
-        source: '/[posturl]',
+        source: 'https://byldgroup.com/:path*',
         destination: 'https://blog.byldgroup.com',
         permanent: true,
       },
