4d3a7c0ea81f277ada60681451a2c9c0b68a4de7
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2023-09-21 15:11:11 +0530

    Update workshops.js
    add workshop.

--- pages/cruciallifechangingskills/workshops.js
+++ pages/cruciallifechangingskills/workshops.js
@@ -68,7 +68,7 @@ export default function Workshops(){
                     
                     
 
-                    {/* <div class="col-md-4 workshop-col"> 
+                   {/* <div class="col-md-4 workshop-col"> 
                        <div class="row workshop-row"> 
                             <div class="col-md-12 workshop-content"> 
                             <img class="left-image img100px mbb-30" src="/classets/img/logo-influencer.svg" alt="" />
@@ -179,7 +179,7 @@ export default function Workshops(){
                        </div>
                     </div> */}
 
-            
+{/*             
                     <div class="col-md-4 workshop-col"> 
                        <div class="row workshop-row"> 
                             <div class="col-md-12 workshop-content gtdcolor"> 
@@ -209,7 +209,7 @@ export default function Workshops(){
                             <a href='#registered'><button class="register">REGISTER NOW</button></a>
                             </div>
                        </div>
-                    </div>
+                    </div> */}
 
                     
                     <div class="col-md-4 workshop-col">
@@ -244,7 +244,7 @@ export default function Workshops(){
                             </div>
                         </div>
 
-                    <div class="col-md-4 workshop-col"> 
+                    {/* <div class="col-md-4 workshop-col"> 
                        <div class="row workshop-row"> 
                             <div class="col-md-12 workshop-content phothemecolor"> 
                             <img class="left-image img100px mbb-30" src="/classets/img/logo-influencer.svg" alt="" />
@@ -257,9 +257,9 @@ export default function Workshops(){
                             <a href='#registered'><button class="register phothemecolor">REGISTER NOW</button></a>
                             </div>
                        </div>
-                    </div>
+                    </div> */}
 
-                    <div class="col-md-4 workshop-col"> 
+                    {/* <div class="col-md-4 workshop-col"> 
                        <div class="row workshop-row"> 
                             <div class="col-md-12 workshop-content"> 
                             <img class="left-image img100px mbb-30" src="/classets/img/logo-influencer.svg" alt="" />
@@ -273,8 +273,8 @@ export default function Workshops(){
                             <a href='#registered'><button class="register">REGISTER NOW</button></a>
                             </div>
                        </div>
-                    </div>
-     <div class="col-md-4 workshop-col">
+                    </div> */}
+                          <div class="col-md-4 workshop-col">
                             <div class="row workshop-row">
                                 <div class="col-md-12 workshop-content green-col">
                                     <img class="left-image img100px mbb-30" src="/classets/img/logo-influencer.svg" alt="" />
@@ -304,7 +304,7 @@ export default function Workshops(){
                        </div>
                     </div>
 
-                    <div class="col-md-4 workshop-col"> 
+                    {/* <div class="col-md-4 workshop-col"> 
                        <div class="row workshop-row"> 
                             <div class="col-md-12 workshop-content"> 
                             <img class="left-image img100px mbb-30" src="/classets/img/logo-influencer.svg" alt="" />
@@ -313,14 +313,30 @@ export default function Workshops(){
                             </div>
                             <h5 className='eventt'>Workshop</h5>
                              <div class="col-md-12 workshop-date"> 
-                            <h6><i class="fa fa-calendar" aria-hidden="true"></i> January 30th - 31st, 2024</h6>
+                            <h6><i class="fa fa-calendar" aria-hidden="true"></i> February 28th - 29th, 2024</h6>
                             <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 AM to 5:00 PM</h6>
                             <a href='#registered'><button class="register">REGISTER NOW</button></a>
                             </div>
                        </div>
-                    </div>
+                    </div> */}
 
-                    <div class="col-md-4 workshop-col"> 
+                               <div class="col-md-4 workshop-col"> 
+                       <div class="row workshop-row"> 
+                            <div class="col-md-12 workshop-content"> 
+                            <img class="left-image img100px mbb-30" src="/classets/img/logo-influencer.svg" alt="" />
+                            <h4>CRUCIAL CONVERSATIONS</h4>
+                            <p>for Mastering Dialogue</p>
+                            </div>
+                            <h5 className='eventt'>Workshop</h5>
+                             <div class="col-md-12 workshop-date"> 
+                            <h6><i class="fa fa-calendar" aria-hidden="true"></i> February 28th - 29th, 2024</h6>
+                            <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 AM to 5:00 PM</h6>
+                            <a href='#registered'><button class="register">REGISTER NOW</button></a>
+                            </div>
+                       </div>
+                    </div> 
+
+                    {/* <div class="col-md-4 workshop-col"> 
                        <div class="row workshop-row"> 
                             <div class="col-md-12 workshop-content green-col"> 
                             <img class="left-image img100px mbb-30" src="/classets/img/logo-influencer.svg" alt="" />
@@ -334,7 +350,7 @@ export default function Workshops(){
                             <a href='#registered'><button class="register">REGISTER NOW</button></a>
                             </div>
                        </div>
-                    </div>
+                    </div> */}
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="row workshop-row"> 
@@ -430,20 +446,13 @@ export default function Workshops(){
                                     <div class="col-md-6 col-6">                                        
                                         <select name='slot' required>
                                             <option value="">Pick any Slot*</option>                                            
-                                            {/* <option value="The Power of Habit - August 24th, 2023">The Power of Habit - August 24th, 2023</option>
-                                            <option value="Influencer - August 24th - 25th, 2023">Influencer - August 24th - 25th, 2023</option> */}
-                                             <option value="Crucial Conversation for Accountability - October 12th - 13th, 2023">Crucial Conversation for Accountability - October 12th - 13th, 2023</option>
+                                           <option value="Crucial Conversation for Accountability - October 12th - 13th, 2023">Crucial Conversation for Accountability - October 12th - 13th, 2023</option>
                                             <option value="The Power of Habit - November 23th, 2023">The Power of Habit - November 23th, 2023</option>
-                                                                                       <option value="Influencer - December 14th - 15th, 2023">Influencer - December 14th - 15th, 2023</option> 
-                 
-                                            <option value="Getting Things Done - September 28th, 2023">Getting Things Done - September 28th, 2023</option>
-                                            <option value="Crucial Conversations for Accountability - October 5th - 6th, 2023">Crucial Conversations for Accountability - October 5th - 6th, 2023</option>                                            
-                                            <option value="The Power of Habit - November 29th, 2023">The Power of Habit - November 29th, 2023</option>
-                                            <option value="Crucial Conversations for Mastering Dialogue - December 14th - 15th, 2023">Crucial Conversations for Mastering Dialogue - December 14th - 15th, 2023</option>
-                                            <option value="Getting Things Done - January 11th, 2024">Getting Things Done - January 11th, 2024</option>
-                                            <option value="Crucial Conversations for Accountability - January 30th - 31st, 2024">Crucial Conversations for Accountability - January 30th - 31st, 2024</option>
-                                            <option value="Influencer - February 28th - 29th, 2024">Influencer - February 28th - 29th, 2024</option>
+                                            <option value="Influencer - December 14th - 15th, 2023">Influencer - December 14th - 15th, 2023</option> 
+                                            <option value="Getting Things Done - January 11th, 2023">Getting Things Done - January 11th, 2023</option>
+                                            <option value="Crucial Conversations for Accountability - February 28th - 29th, 2024">Crucial Conversations for Mastering Dialogue - February 28th - 29th, 2024</option>                                            
                                             <option value="The Power of Habit - March 14th, 2024">The Power of Habit - March 14th, 2024</option>
+                                           
                                         </select>
                                     </div>
                                     <div class="col-md-6 col-6">                                        
