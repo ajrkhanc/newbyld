8568299e5ce9cdd97cc32bd71abe636f5261bd2d
ajrkhan1 <102628339+ajrkhanc@users.noreply.github.com>
2023-09-27 08:21:13 +0530

    ds
    sd

--- pages/blanchardindia/index.js
+++ pages/blanchardindia/index.js
@@ -10,8 +10,8 @@ export default function Home() {
       arrows: false,
       infinite: true,
       speed: 500,
-      slidesToShow: 5,
-      slidesToScroll: 5,
+      slidesToShow: 6,
+      slidesToScroll: 6,
       initialSlide: 0,
       autoplay: false,
       autoplaySpeed: 3000,
@@ -19,8 +19,8 @@ export default function Home() {
          {
             breakpoint: 1024,
             settings: {
-               slidesToShow: 3,
-               slidesToScroll: 3,
+               slidesToShow: 4,
+               slidesToScroll: 4,
                infinite: true,
                dots: true
             }
