045ad753c7eba3ed9f51a7e987ef25e62e518469
ajrkhan1 <102628339+ajrkhanc@users.noreply.github.com>
2023-09-29 11:15:24 +0530

    kk
    jj

--- pages/_app.js
+++ pages/_app.js
@@ -1071,7 +1071,7 @@ export default function MyApp({ Component, pageProps, router }) {
 
   }
 
-  else if (router.pathname.startsWith('/eq/eq-assessment/')) {
+  else if (router.pathname.startsWith('/eq/eq-assessment1/')) {
 
     return (
       <DTCIASSESSMENT>
@@ -1126,6 +1126,61 @@ export default function MyApp({ Component, pageProps, router }) {
 
   }
   else if (router.pathname.startsWith('/eq/eq-assessment2/')) {
+
+    return (
+      <DTCIASSESSMENT>
+        <Head>
+          <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" />
+
+          <link rel="stylesheet" type="text/css" href="/assets/css/font-awesome.min.css" />
+
+          <link rel="stylesheet" type="text/css" href="/assets/fonts/flaticon.css" />
+
+          <link rel="stylesheet" type="text/css" href="/assets/css/animate.css" />
+
+          <link rel="stylesheet" type="text/css" href="/assets/css/owl.carousel.css" />
+
+          <link rel="stylesheet" type="text/css" href="/assets/css/off-canvas.css" />
+
+          <link rel="stylesheet" type="text/css" href="/assets/css/magnific-popup.css" />
+
+          <link rel="stylesheet" href="/assets/css/rsmenu-main.css" />
+
+          <link rel="stylesheet" type="text/css" href="/assets/inc/custom-slider/css/nivo-slider.css" />
+          <link rel="stylesheet" type="text/css" href="/assets/inc/custom-slider/css/preview.css" />
+
+          <link rel="stylesheet" type="text/css" href="/assets/css/rs-spacing.css" />
+
+          <link rel="stylesheet" type="text/css" href="/assets/css/style.css" />
+
+          <link rel="stylesheet" type="text/css" href="/assets/css/responsive.css" />
+          <link rel="stylesheet" type="text/css" href="/assets/css/globals.css" />
+          <link rel="stylesheet" type="text/css" href="/assets/css/bg.css" />
+
+          <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+          <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-5PXTTR960E"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5PXTTR960E');
+          `,
+            }}
+          />
+
+        </Head>
+
+        <Component {...pageProps} />
+      </DTCIASSESSMENT>
+    )
+
+  }
+
+  else if (router.pathname.startsWith('/eq/eq-assessments/')) {
 
     return (
       <DTCIASSESSMENT>
