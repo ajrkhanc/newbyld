44b4c089a79b99c5738fed6682617c986c40f3d8
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2023-10-31 15:23:49 +0530

    Update blanchard-management-essentials.js
    ok

--- pages/blanchardindia/lp/blanchard-management-essentials.js
+++ pages/blanchardindia/lp/blanchard-management-essentials.js
@@ -102,7 +102,7 @@ export default function BlanchardManaE() {
                            <label>Interested In*</label>
                            <select name="interest_in" className="form-control" required>
                               <option value="">---Select---</option>
-                              <option value="Blanchard Management Essentials®  20th December, 2023">Blanchard Management Essentials® Workshop 13th September 2023</option>
+                              <option value="Blanchard Management Essentials®  20th December, 2023">Blanchard Management Essentials® Workshop 20th December 2023</option>
                            </select>
                            <div className="help-block with-errors"></div>
                         </div>
