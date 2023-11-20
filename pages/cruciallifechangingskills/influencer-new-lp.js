07cb1f71ac755018c82cec569a1bfcb3453e3640
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2023-11-20 10:39:38 +0530

    Update influencer-new-lp.js
    date changes

--- pages/cruciallifechangingskills/influencer-new-lp.js
+++ pages/cruciallifechangingskills/influencer-new-lp.js
@@ -158,7 +158,7 @@ export default function BrowseCourses(){
                             
                                      {/* <option value="Virtual Course Preview - November 30th, 2023">Virtual Course Preview - November 30th, 2023</option> */}
                                         <option value="Virtual Course Preview - November 30th, 2023">Virtual Course Preview - November 30th, 2023</option>
-                                        <option value="Workshop - December 14th - 15th, 2023">Workshop - December 14th - 15th, 2023</option>
+                                        <option value="Workshop - December 14th  2023">Workshop - December 14th  2023</option>
                                         {/* <option value="Workshop - February 28th - 29th, 2024">Workshop - February 28th - 29th, 2024</option> */}
                         </select>
                         </div>
@@ -274,7 +274,7 @@ export default function BrowseCourses(){
                                 </div>
                                 <h5 className='eventt'>Workshop</h5>
                                 <div class="col-md-12 workshop-date green-col">
-                                    <h6><i class="fa fa-calendar" aria-hidden="true"></i> December 14th - 15th, 2023</h6>
+                                    <h6><i class="fa fa-calendar" aria-hidden="true"></i> December 14th 2023</h6>
                                     <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 AM to 5:00 PM</h6>
                                     <a href='#registered'><button class="register">REGISTER NOW</button></a>
                                 </div>
