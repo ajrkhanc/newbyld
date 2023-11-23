a5ec161524f339b55a4524087de03687eee9a718
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2023-11-23 17:36:49 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -644,11 +644,7 @@ export default function MyApp({ Component, pageProps, router }) {
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <link rel="icon" href="/efassets/favicon.png" />
         
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WF8WJWJ');</script>
+
 
           <link rel="stylesheet" type="text/css" href="/wileyassets/css/bootstrap.min.css" />
 
