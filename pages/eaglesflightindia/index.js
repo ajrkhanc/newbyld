2f24f8f00ea3edfcc507941e1f6f5ebdb169f44b
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2023-11-23 16:26:25 +0530

    updated gtag manager code

--- pages/eaglesflightindia/index.js
+++ pages/eaglesflightindia/index.js
@@ -194,8 +194,21 @@ export default function Home() {
                }}
             />
 
+               <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WF8WJWJ');</script>
+<!-- End Google Tag Manager -->
+
 <link rel="stylesheet" type="text/css" href="/efassets/css/homemodule.css" />
          </Head>
+
+               <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WF8WJWJ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
          <section className='bannersection'>
             <Slider {...homeslider}>
