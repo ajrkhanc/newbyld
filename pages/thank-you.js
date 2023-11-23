6590c2511b1a68ab813aec836963c98d712f9bfa
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2023-11-23 16:56:09 +0530

    Update thank-you.js

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -8,22 +8,11 @@ export default function ThankYou(){
             <meta name="description" content="BYLD is a pioneering name in leadership development, corporate training, assessment, and staffing. Founded in 1998, the company is the largest group in South Asia offering HR and business productivity solutions for individuals, teams, and organizations. It has also served 300 of 500 Fortune companies."/> 
         <!-- Google tag (gtag.js) --> 
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-R93RP8FMVC"></script> 
-        <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-R93RP8FMVC'); 
- 
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WF8WJWJ');</script>
-
-        
+        <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-R93RP8FMVC');     
     </script>  
         </Head>
 
         
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WF8WJWJ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
     
           <div className="rs-services  ptt-40 pbb-40">
             <div className="container">
