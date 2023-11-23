eb88cc86e9d0c023bcf994498f7d799291c73849
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2023-11-23 16:53:12 +0530

    Update thank-you.js

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -9,21 +9,21 @@ export default function ThankYou(){
         <!-- Google tag (gtag.js) --> 
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-R93RP8FMVC"></script> 
         <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-R93RP8FMVC'); 
-    <!-- Google Tag Manager -->
+ 
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WF8WJWJ');</script>
-<!-- End Google Tag Manager -->
+
         
     </script>  
         </Head>
 
-        <!-- Google Tag Manager (noscript) -->
+        
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WF8WJWJ"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+
     
           <div className="rs-services  ptt-40 pbb-40">
             <div className="container">
