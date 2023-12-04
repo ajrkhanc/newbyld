387fe5505412256608af76c6e6b6ca29cfb64a00
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2023-12-04 17:17:04 +0530

    Update getting-things-done-new-lp.js
    add date

--- pages/cruciallifechangingskills/getting-things-done-new-lp.js
+++ pages/cruciallifechangingskills/getting-things-done-new-lp.js
@@ -214,21 +214,21 @@ export default function Gettingthingsdone() {
                             <br></br>
                             <Slider {...settings2}>
                               
-                                 <div>
+                                 {/* <div>
                                     <h2>Workshop</h2>
                                     <div class="workshop-date">
                                         <h6><i class="fa fa-calendar" aria-hidden="true"></i> November 6th - 8th, 2023</h6>
                                         <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 02:30 PM to 5:00 PM</h6>
                                     </div>
-                                </div>
+                                </div> */}
 
-                                <div>
+                                {/* <div>
                                     <h2>Train-The-Trainers (TTT) - Workshop</h2>
                                     <div class="workshop-date">
                                         <h6><i class="fa fa-calendar" aria-hidden="true"></i> November 9th - 10th, 2023</h6>
                                         <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 AM to 4:00 PM</h6>
                                     </div>
-                                </div>
+                                </div> */}
 
                                 <div>
                                     <h2>Virtual Course Preview</h2>
@@ -237,12 +237,33 @@ export default function Gettingthingsdone() {
                                         <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 11:00 AM to 12:00 PM</h6>
                                     </div>
                                 </div>
-
+                                <div>
+                                    <h2>Virtual Course Preview</h2>
+                                    <div class="workshop-date">
+                                        <h6><i class="fa fa-calendar" aria-hidden="true"></i> January 5th, 2024</h6>
+                                        <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 11:00 AM to 12:00 PM</h6>
+                                    </div>
+                                </div>
                                 <div>
                                     <h2>Workshop</h2>
                                     <div class="workshop-date">
                                         <h6><i class="fa fa-calendar" aria-hidden="true"></i> January 11th, 2024</h6>
                                         <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 AM to 5:00 PM</h6>
+                                    </div>
+                                </div>
+                                <div>
+                                    <h2>Workshop</h2>
+                                    <div class="workshop-date">
+                                        <h6><i class="fa fa-calendar" aria-hidden="true"></i>February 9th, 2024</h6>
+                                        <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 AM to 5:00 PM</h6>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <h2>Virtual Course Preview</h2>
+                                    <div class="workshop-date">
+                                        <h6><i class="fa fa-calendar" aria-hidden="true"></i>March 14th, 2024</h6>
+                                        <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 11:00 AM to 12:00 PM</h6>
                                     </div>
                                 </div>
                             </Slider>
@@ -271,10 +292,17 @@ export default function Gettingthingsdone() {
                                 <div class="col-md-12">
                                     <select name='slot' required>                                                                              
                                         <option value="">Pick any Slot*</option>
-                                            <option value="Getting Things Done - November 6th to 8th, 2023">Getting Things Done - November 6th to 8th, 2023</option>
-                                            <option value="Getting Things Done - November 9th to 10th, 2023">Getting Things Done - November 9th to 10th, 2023</option>
+                                            {/* <option value="Getting Things Done - November 6th to 8th, 2023">Getting Things Done - November 6th to 8th, 2023</option>
+                                            <option value="Getting Things Done - November 9th to 10th, 2023">Getting Things Done - November 9th to 10th, 2023</option> */}
                                             <option value="Getting Things Done, Virtual Course Preview -  December 21st, 2023">Getting Things Done, Virtual Course Preview - December 21st, 2023</option>
+                                            <option value="Getting Things Done, Virtual Course Preview -  January 5th, 2024">Getting Things Done, Virtual Course Preview - January 5th, 2024</option>
                                             <option value="Getting Things Done - January 11th, 2024">Getting Things Done - January 11th, 2024</option>
+
+                                            <option value="Getting Things Done, Workshop -  February 9th, 2024">Getting Things Done, Workshop - February 9th, 2024</option>
+
+                                            <option value="Getting Things Done, Virtual Course Preview -  March 14th, 2024">Getting Things Done, Virtual Course Preview - March 14th, 2024</option>
+
+
                                     </select>
                                 </div>
 
