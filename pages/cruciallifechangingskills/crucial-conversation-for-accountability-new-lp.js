ec1f21d010883d048a8bba2d2daa42cb90f4a870
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-18 15:57:24 +0530

    Update crucial-conversation-for-accountability-new-lp.js
    update

--- pages/cruciallifechangingskills/crucial-conversation-for-accountability-new-lp.js
+++ pages/cruciallifechangingskills/crucial-conversation-for-accountability-new-lp.js
@@ -158,10 +158,10 @@ export default function BrowseCourses() {
                                 <div class="col-md-6 col-6">
                                     <select name='slot' required>
                                         <option value="">Pick any Slot*</option>                                        
-                                         {/*<option value="Virtual Course Preview – 30th January, 2024">Virtual Course Preview – 30th January, 2024</option> */} 
+                                   
                                         <option value="Workshop–4th-&-5th-March,2024">Workshop– 4th & 5th March, 2024</option>
 
-                                       {/* <option value="Crucial Conversations for Accountability - January 30th - 31st, 2024">Crucial Conversations for Accountability - January 30th - 31st, 2024</option> */}
+                                    
                                     </select>
                                 </div>
 
@@ -223,22 +223,7 @@ export default function BrowseCourses() {
 
                       
 
-                       {/*<div class="col-md-4 workshop-col">
-                            <div class="row workshop-row">
-                                <div class="col-md-12 workshop-content">
-                                    <img class="left-image img100px mbb-30" src="/classets/img/logo-conversations.svg" alt="" />
-                                    <h4>CRUCIAL CONVERSATIONS</h4>
-                                    <p>for Accountability</p>
-                                </div>
-                                <h5 className='eventt'>Virtual Course Preview</h5>
-                                <div class="col-md-12 workshop-date">
-                                    <h6><i class="fa fa-calendar" aria-hidden="true"></i> 30th January, 2024</h6>
-                                    <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 AM to 5:00 PM</h6>
-                                    {/* <p className='soldout'>Sold Out</p> */}
-                                    <a href='#registered'><button class="register">REGISTER NOW</button></a>
-                                </div>
-                            </div>
-                        </div> */}
+                      
                         
                         <div class="col-md-4 workshop-col">
                             <div class="row workshop-row">
