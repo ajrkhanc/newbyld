cce4d7fe320006ce96a5ef51c053ac8b5385dc89
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-19 15:34:41 +0530

    Update course-overview-the-power-of-habit.js
    update canonical

--- pages/cruciallifechangingskills/course-overview-the-power-of-habit.js
+++ pages/cruciallifechangingskills/course-overview-the-power-of-habit.js
@@ -41,6 +41,7 @@ export default function Courseoverviewcrucialconversationsformasteringdialogue()
         <>
         <Head>
             <title>Course Overview - The Power of Habit - Crucial Life-Changing Skills</title>
+        <link rel="canonical" href="https://byldgroup.com/cruciallifechangingskills/course-overview-the-power-of-habit" />
             <meta name="description" content=""/> 
         </Head>
         <section class="about-section workshop-hero">
