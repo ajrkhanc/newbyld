fed4bdaa6d1d9804a6667219d2b9c5be526375b6
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-23 17:46:45 +0530

    Update Header.js
    Update header

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -115,7 +115,7 @@ export default function Header() {
                                                                     <li><a target="_blank" href="/everythingdisc">Everything DiSC (A Wiley Brand)</a> </li>
                                                                     <li><a target="_blank" href="/fivebehaviors">Five Behaviors</a> </li>
                                                                     <li><a href="/assessments/lumina-psychometric-assesments">Lumina Spark</a></li>
-                                                                    <li><a href="/assessments/talentsmart-eq-assesments">TalentSmart EQ</a></li>
+                                                                    {/* <li><a href="/assessments/talentsmart-eq-assesments">TalentSmart EQ</a></li> */}
                                                                     <li className='d-none'><a href="/assessments/assessments-and-development-centers">Assessments and Development Centers</a></li>
                                                                 </ul>
                                                             </li>
@@ -320,7 +320,7 @@ export default function Header() {
                                             <li><a target="_blank" href="/everythingdisc">Everything DiSC (A Wiley Brand)</a> </li>
                                             <li><a target="_blank" href="/fivebehaviors">Five Behaviors</a> </li>
                                             <li><a href="/assessments/lumina-psychometric-assesments">Lumina Spark</a></li>
-                                            <li><a href="/assessments/talentsmart-eq-assesments">TalentSmart EQ</a></li>
+                                             {/* <li><a href="/assessments/talentsmart-eq-assesments">TalentSmart EQ</a></li> */}
                                             <li className='d-none'><a href="/assessments/assessments-and-development-centers">Assessments and Development Centers</a></li>
                                         </ul>
                                     </li>
