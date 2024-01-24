b7558a030ff21df4f2b7af15c1e2ca56aa34d3f6
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-24 17:50:48 +0530

    Update change-agility.js
    add canonical

--- pages/blanchardindia/our-content/challenges/change-agility.js
+++ pages/blanchardindia/our-content/challenges/change-agility.js
@@ -46,6 +46,7 @@ export default function ThanYouLP() {
         <>
             <Head>
                 <title>Change & Agility| Blanchard International</title>
+                <link rel="canonical" href="https://byldgroup.com/blanchardindia/our-content/challenges/change-agility" />
                 <meta name="description" content="Learn the core capabilities today’s leaders need to be successful and how Blanchard’s programs can develop your organization’s leadership capability." />
                 <link rel="icon" href="/blanchardindia/favicon.ico" />
             </Head>
