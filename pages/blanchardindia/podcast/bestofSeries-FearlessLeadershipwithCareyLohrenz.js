9925ea349d0579b2e1c73529fba2a59eb5e002a0
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-25 12:30:18 +0530

    Update bestofSeries-FearlessLeadershipwithCareyLohrenz.js
    Add canonical

--- pages/blanchardindia/podcast/bestofSeries-FearlessLeadershipwithCareyLohrenz.js
+++ pages/blanchardindia/podcast/bestofSeries-FearlessLeadershipwithCareyLohrenz.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
     <>
       <Head>
         <title>Podcast - Love + Work with Marcus Buckingham</title>
+    <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/bestofSeries-FearlessLeadershipwithCareyLohrenz" />
         <meta
           name="description"
           content="Hear Christina Maslach talk about how to recognize the signs of employee burnout and how to combat the issue to promote increased productivity and health."
