0a6dbbebf7a68b774e2b4ab44617a04e7992e611
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-25 12:22:58 +0530

    Update humbitious-the-power-of-low-ego-high-drive-leadership-with-amer-kaissi.js
    Add canonical

--- pages/blanchardindia/podcast/humbitious-the-power-of-low-ego-high-drive-leadership-with-amer-kaissi.js
+++ pages/blanchardindia/podcast/humbitious-the-power-of-low-ego-high-drive-leadership-with-amer-kaissi.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - Humbitious: The Power of Low-Ego, High-Drive Leadership with Amer Kaissi</title>
+        <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/humbitious-the-power-of-low-ego-high-drive-leadership-with-amer-kaissi" />
         <meta name="description" content="In this episode Amer Kaissi shares results of his extensive research into the traits that define strong, effective leaders from his new book"/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
