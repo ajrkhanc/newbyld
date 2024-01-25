3d97175cafa8410c8a1661e6ce863477ecdb053d
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-25 12:43:55 +0530

    Update the-no-os-of-leadership-with-mike-easley.js
    Add canonical

--- pages/blanchardindia/podcast/the-no-os-of-leadership-with-mike-easley.js
+++ pages/blanchardindia/podcast/the-no-os-of-leadership-with-mike-easley.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - The No-Os of Leadership with Mike Easley</title>
+        <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/the-no-os-of-leadership-with-mike-easley" />
         <meta name="description" content="In this episode Amer Kaissi shares results of his extensive research into the traits that define strong, effective leaders from his new book"/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
