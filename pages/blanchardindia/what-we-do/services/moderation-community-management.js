bf66c797c6ebf7357691c1e64fca401fbbe0b79d
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-25 12:47:13 +0530

    Update moderation-community-management.js
    Add canonical

--- pages/blanchardindia/what-we-do/services/moderation-community-management.js
+++ pages/blanchardindia/what-we-do/services/moderation-community-management.js
@@ -6,6 +6,7 @@ export default function ThanYouLP() {
         <>
             <Head>
                 <title>Moderation & Community Management | Blanchard International</title>
+                <link rel="canonical" href="https://byldgroup.com/blanchardindia/what-we-do/services/moderation-community-management" />
                 <meta name="description" content="Learn how Blanchard’s approach to learning sustainment and reinforcement keeps learners engaged and empowers them to apply critical concepts in the workplace."/>
                 <link rel="icon" href="/blanchardindia/favicon.ico" />
             </Head>
