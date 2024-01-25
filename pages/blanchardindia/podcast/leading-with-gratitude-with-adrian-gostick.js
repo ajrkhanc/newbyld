f6553603a9690645835523309c47149d1d58b766
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-25 12:13:43 +0530

    Update leading-with-gratitude-with-adrian-gostick.js
    Add canonical

--- pages/blanchardindia/podcast/leading-with-gratitude-with-adrian-gostick.js
+++ pages/blanchardindia/podcast/leading-with-gratitude-with-adrian-gostick.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - Leading with Gratitude with Adrian Gostick</title>
+        <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/leading-with-gratitude-with-adrian-gostick" />
         <meta name="description" content="Hear Christina Maslach talk about how to recognize the signs of employee burnout and how to combat the issue to promote increased productivity and health."/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
