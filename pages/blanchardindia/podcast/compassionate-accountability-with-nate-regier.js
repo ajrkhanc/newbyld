e8293e4dec76f34e4fbd4eca409f38637ea1ce99
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-25 12:53:19 +0530

    Update compassionate-accountability-with-nate-regier.js
    Add canonical

--- pages/blanchardindia/podcast/compassionate-accountability-with-nate-regier.js
+++ pages/blanchardindia/podcast/compassionate-accountability-with-nate-regier.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - Compassionate Accountability with Nate Regier</title>
+        <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/compassionate-accountability-with-nate-regier" />
         <meta name="description" content="Hear Christina Maslach talk about how to recognize the signs of employee burnout and how to combat the issue to promote increased productivity and health."/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
