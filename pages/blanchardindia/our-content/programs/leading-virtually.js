168e88d3a709a0c5e25e513218685190eb898c58
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-26 12:28:04 +0530

    Update leading-virtually.js

--- pages/blanchardindia/our-content/programs/leading-virtually.js
+++ pages/blanchardindia/our-content/programs/leading-virtually.js
@@ -10,9 +10,80 @@ export default function ThanYouLP() {
         <>
             <Head>
                 <title>Leading Virtually Program | Virtual Team Management Training Program</title>
+                <meta name="robots" content="INDEX, FOLLOW" />
                 <link rel="canonical" href="https://byldgroup.com/blanchardindia/our-content/programs/leading-virtually" />
                 <meta name="description" content="Blanchard's Leading Virtually training program teaches people how to lead remote teams that thrive. Train your leaders for virtual and hybrid teams to provide extra support, boundaries, and norms." />
                 <link rel="icon" href="/blanchardindia/favicon.ico" />
+                {/* og tag */}
+                <meta property="og:url" content="https://byldgroup.com/blanchardindia/our-content/programs/leading-virtually" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Leading Virtually Program | Virtual Team Management Training Program" />
+                <meta property="og:description" content="Blanchard's Leading Virtually training program teaches people how to lead remote teams that thrive. Train your leaders for virtual and hybrid teams to provide extra support, boundaries, and norms." />
+                <meta property="og:image" content="https://byldgroup.com/kbassets/img/Blanchard_Global%20Partner%20Logo_India-01.png"></meta>
+
+
+                {/* TWITTER TAG */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@Blanchard_India" />
+                <meta name="twitter:title" content="Leading Virtually Program | Virtual Team Management Training Program" />
+                <meta name="twitter:description" content="Blanchard's Leading Virtually training program teaches people how to lead remote teams that thrive. Train your leaders for virtual and hybrid teams to provide extra support, boundaries, and norms." />
+                <meta name="twitter:image:src" content="https://pbs.twimg.com/profile_images/1729446806688333824/WrhGIYA-_400x400.jpg" />
+                <meta name="twitter:domain" content="https://byldgroup.com/blanchardindia/our-content/programs/leading-virtually" />
+
+
+                {/* itemProp TAG */}
+                <meta itemProp="title" content="Leading Virtually Program | Virtual Team Management Training Program" />
+                <meta itemProp="description" content="Blanchard's Leading Virtually training program teaches people how to lead remote teams that thrive. Train your leaders for virtual and hybrid teams to provide extra support, boundaries, and norms." />
+                <meta itemProp="image" content="https://byldgroup.com/kbassets/img/Blanchard_Global%20Partner%20Logo_India-01.png" />
+
+                {/* script schema */}
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            "@context": "https://schema.org/",
+                            "@type": "WebSite",
+                            "name": "Blanchard India",
+                            "url": "https://byldgroup.com/blanchardindia/",
+                            "potentialAction": {
+                                "@type": "SearchAction",
+                                "target": "https://byldgroup.com/blanchardindia/our-content/programs/leading-virtually{search_term_string}",
+                                "query-input": "required name=search_term_string"
+                            }
+                        }),
+                    }}
+                />
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            "@context": "https://schema.org",
+                            "@type": "Organization",
+                            "name": "Blanchard Research and Training India",
+                            "alternateName": "Blanchard India",
+                            "url": "https://byldgroup.com/blanchardindia",
+                            "logo": "https://byldgroup.com/kbassets/img/Blanchard_Global%20Partner%20Logo_India-01.png",
+                            "contactPoint": {
+                                "@type": "ContactPoint",
+                                "telephone": "18001021345",
+                                "contactType": "customer service",
+                                "contactOption": "TollFree",
+                                "areaServed": "IN",
+                                "availableLanguage": "en"
+                            },
+                            "sameAs": [
+                                "https://www.facebook.com/blanchardresearchandtrainingindia",
+                                "https://twitter.com/blanchard_india",
+                                "https://www.instagram.com/blanchard_research_india/",
+                                "https://www.youtube.com/c/BlanchardinternationalIndia",
+                                "https://www.linkedin.com/company/blanchard-research-and-training-india/",
+                                "https://byldgroup.com/blanchardindia"
+                            ]
+                        }),
+                    }}
+                />
+
+
             </Head>
 
             <section className="bredkums2 colortheme4bg pbb-100 border-bottom-left-radius-70">
@@ -40,15 +111,15 @@ export default function ThanYouLP() {
                     <div className="row align-items-center">
                         <div className="col-sm-6">
                             <h2 className="sectionh">
-                            Leadership is Equally Important for Remote Teams
+                                Leadership is Equally Important for Remote Teams
                             </h2>
                             <p className="headingp">
-                            Increasingly, today's workforce works from home and other remote locations, with teams spread across countries and time zones. Due to time lags and technology interfaces, managers need help connecting with their people. It's the new normal, with several benefits and challenges.
+                                Increasingly, today's workforce works from home and other remote locations, with teams spread across countries and time zones. Due to time lags and technology interfaces, managers need help connecting with their people. It's the new normal, with several benefits and challenges.
                             </p>
                             <p className="headingp">
-                            Managing in this environment is much more challenging than working together in the same physical space. Understanding what your employees need and building rapport and trust over long distances can be difficult. Managers can increase the effectiveness and productivity of their remote staff by learning Leading Virtually™
+                                Managing in this environment is much more challenging than working together in the same physical space. Understanding what your employees need and building rapport and trust over long distances can be difficult. Managers can increase the effectiveness and productivity of their remote staff by learning Leading Virtually™
                             </p>
-                           
+
                             <a href='/blanchardindia/get-started' className='kbbtn kbbtn--orange mtt-20'>Talk to an Expert</a>
                         </div>
                         <div className="col-sm-6">
@@ -72,16 +143,16 @@ export default function ThanYouLP() {
                         <div className="col-sm-6">
                             <h2 className="sectionh">Learn the formula for engaged virtual workers</h2>
                             <p className="headingp text-justify">
-                            This course provides learners a highly interactive experience that includes instruction, responses, reflection, group activities, and opportunities to practice new skills. The program focuses on three key practices proven to boost manager effectiveness in virtual teams, based on almost 20 years of assisting managers lead virtual teams.
+                                This course provides learners a highly interactive experience that includes instruction, responses, reflection, group activities, and opportunities to practice new skills. The program focuses on three key practices proven to boost manager effectiveness in virtual teams, based on almost 20 years of assisting managers lead virtual teams.
                             </p>
                             <p className="headingp  text-justify">
-                            To Be Attentive and Mindful, managers must communicate intentionally with remote team members, structure conversations for maximum impact, and honor each other's work preferences.
+                                To Be Attentive and Mindful, managers must communicate intentionally with remote team members, structure conversations for maximum impact, and honor each other's work preferences.
                             </p>
                             <p className="headingp  text-justify">
-                            Using technology, leaders learn how to foster community by building trusting, supportive relationships that involve all their people and stay positive.
+                                Using technology, leaders learn how to foster community by building trusting, supportive relationships that involve all their people and stay positive.
                             </p>
                             <p className="headingp  text-justify">
-                            Lastly, managers learn how to Accelerate Performance and Development by helping remote workers identify a career path and move forward.
+                                Lastly, managers learn how to Accelerate Performance and Development by helping remote workers identify a career path and move forward.
                             </p>
                         </div>
 
@@ -100,18 +171,18 @@ export default function ThanYouLP() {
                         <div className="col-sm-10 text-center pbb-40">
                             <h2>Delivery Options to Meet Your Needs</h2>
                             <p className="pcl">
-                            We can deliver the Leading Virtually course as three virtual sessions so your managers can learn how to lead their remote teams more effectively wherever they are and whenever they have time.
+                                We can deliver the Leading Virtually course as three virtual sessions so your managers can learn how to lead their remote teams more effectively wherever they are and whenever they have time.
                             </p>
                         </div>
 
                         <div className="col-sm-12">
                             <div className="ourstabs">
                                 <Tabs>
-                                    <TabList className="lefttab">                                       
+                                    <TabList className="lefttab">
                                         <Tab>
                                             <img className="" src="/kbassets/img/kbnew/icon/Virtual.svg" alt="Images" />
                                             <span>Virtual</span>
-                                        </Tab>                                       
+                                        </Tab>
                                     </TabList>
 
                                     <TabPanel className="tabdetails colortheme2bg">
@@ -119,7 +190,7 @@ export default function ThanYouLP() {
                                             <div className="tabinner">
                                                 <h3>Virtual Instructor-led Course</h3>
                                                 <p>
-                                                Leading Virtually is a virtual instructor-led course that unfolds over three two-hour sessions. The virtual sessions offer learners a highly engaging experience that includes instruction, responses, reflection, group activities, and opportunities to practice new skills in a safe setting. The program also allows learners to share stories and best practices in their transition to managing others virtually.
+                                                    Leading Virtually is a virtual instructor-led course that unfolds over three two-hour sessions. The virtual sessions offer learners a highly engaging experience that includes instruction, responses, reflection, group activities, and opportunities to practice new skills in a safe setting. The program also allows learners to share stories and best practices in their transition to managing others virtually.
                                                 </p>
                                                 <p><b>Prework: LAUNCH — </b>Engaging prework assignments, including an assessment, that explain key Leading Virtually concepts</p>
                                                 <p><b>Session 1: LEARN —</b> Be Attentive and Mindful: Teaches leaders how to interact with remote workers and understand what’s harder about managing virtually.</p>
@@ -148,10 +219,10 @@ export default function ThanYouLP() {
                         <div className='col-sm-6'>
                             <div>
                                 <h2>
-                                Make your remote teams more productive
+                                    Make your remote teams more productive
                                 </h2>
                                 <p className="themepbold pbb-10">
-                                On average, remote workers are <b><span>20%</span></b> more productive and half as likely to seek another job as their conventional counterparts
+                                    On average, remote workers are <b><span>20%</span></b> more productive and half as likely to seek another job as their conventional counterparts
                                 </p>
                             </div>
                         </div>
@@ -172,10 +243,10 @@ export default function ThanYouLP() {
                         <div className='col-sm-8 pbb-70'>
                             <div className="text-center">
                                 <h2 className="sectionh">
-                                Leading successful virtual and hybrid teams requires unique leadership skills
+                                    Leading successful virtual and hybrid teams requires unique leadership skills
                                 </h2>
                                 <p>
-                                Employees who work remotely will be more satisfied, engaged, and productive when their leaders practice being attentive and mindful, foster community, and accelerate performance and development.
+                                    Employees who work remotely will be more satisfied, engaged, and productive when their leaders practice being attentive and mindful, foster community, and accelerate performance and development.
                                 </p>
                             </div>
                         </div>
@@ -186,7 +257,7 @@ export default function ThanYouLP() {
                                 <img className="" src="/kbassets/img/kbnew/icon/boost-connection.svg" alt="Images" />
                                 <h3 className="lineh34">Strengthen your connection</h3>
                                 <p>
-                                Strong virtual leaders can boost their team connections through more intentional and structured communication.
+                                    Strong virtual leaders can boost their team connections through more intentional and structured communication.
                                 </p>
                             </div>
                         </div>
@@ -196,7 +267,7 @@ export default function ThanYouLP() {
                                 <img className="" src="/kbassets/img/kbnew/icon/Improve-employee-engagement.svg" alt="Images" />
                                 <h3 className="lineh34">Engage employees more effectively</h3>
                                 <p>
-                                By fostering collaboration, creating a team culture, and building community, great leaders connect remote team members to the larger organization.
+                                    By fostering collaboration, creating a team culture, and building community, great leaders connect remote team members to the larger organization.
                                 </p>
                             </div>
                         </div>
@@ -206,7 +277,7 @@ export default function ThanYouLP() {
                                 <img className="" src="/kbassets/img/kbnew/icon/increase-autonomy-and-empowerment.svg" alt="Images" />
                                 <h3 className="lineh34">Empower & Increase Autonomy</h3>
                                 <p>
-                                To achieve their goals and results consistently, effective virtual leaders increase their people's resourcefulness and autonomy.
+                                    To achieve their goals and results consistently, effective virtual leaders increase their people's resourcefulness and autonomy.
                                 </p>
                             </div>
                         </div>
@@ -219,11 +290,11 @@ export default function ThanYouLP() {
             <section className="ptt-90 ExploreCustom contanerbgc">
                 <div className="container contanerbgc mbordee pbb-60 ptt-60">
                     <div className="row align-items-center">
-                    <div className="col-sm-3"></div>
+                        <div className="col-sm-3"></div>
                         <div className="col-sm-6 text-center">
                             <h2 className="sectionh">Are you ready to get started?</h2>
                             <p className="headingp pbb-10">
-                            Contact us today to learn how we can help your leaders learn the skills they need to lead successful remote teams.
+                                Contact us today to learn how we can help your leaders learn the skills they need to lead successful remote teams.
                             </p>
                             <a href='/blanchardindia/get-started' className='kbbtn kbbtn--orange  mtt-10'>Talk to an Expert</a>
                         </div>
