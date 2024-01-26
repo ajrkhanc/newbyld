14ae89d48dfdd63752334e41d0bbf8d5c9eed2ee
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-26 13:18:01 +0530

    Update servant-leadership-essentials.js

--- pages/blanchardindia/our-content/programs/servant-leadership-essentials.js
+++ pages/blanchardindia/our-content/programs/servant-leadership-essentials.js
@@ -9,10 +9,79 @@ export default function ThanYouLP() {
     return (
         <>
             <Head>
-                <title>Servant Leadership Essentials | Blanchard International</title>
+                <title>Servant Leadership Essentials Training Program | Blanchard India</title>
+                <meta name="robots" content="INDEX, FOLLOW" />
                 <link rel="canonical" href="https://byldgroup.com/blanchardindia/our-content/programs/servant-leadership-essentials" />
-                <meta name="description" content="Blanchard's Servant Leadership course teaches leaders how to think differently about leadership’s purpose and bring out the best in their people." />
+                <meta name="description" content="Join Servant Leadership Essentials Training, hands-on webinars, workshops, leadership books and more. Enquire to Discover how to inspire your team today!" />
                 <link rel="icon" href="/blanchardindia/favicon.ico" />
+                {/* og tag */}
+                <meta property="og:url" content="https://byldgroup.com/blanchardindia/our-content/programs/self-leadership" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Servant Leadership Essentials Training Program | Blanchard India" />
+                <meta property="og:description" content="Join Servant Leadership Essentials Training, hands-on webinars, workshops, leadership books and more. Enquire to Discover how to inspire your team today!" />
+                <meta property="og:image" content="https://byldgroup.com/kbassets/img/Blanchard_Global%20Partner%20Logo_India-01.png" />
+
+                {/* TWITTER TAG */}
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@Blanchard_India" />
+                <meta name="twitter:title" content="Servant Leadership Essentials Training Program | Blanchard India" />
+                <meta name="twitter:description" content="Join Servant Leadership Essentials Training, hands-on webinars, workshops, leadership books and more. Enquire to Discover how to inspire your team today!" />
+                <meta name="twitter:image:src" content="https://pbs.twimg.com/profile_images/1729446806688333824/WrhGIYA-_400x400.jpg" />
+                <meta name="twitter:domain" content="https://byldgroup.com/blanchardindia/our-content/programs/servant-leadership-essentials" />
+                {/* itemProp TAG */}
+                <meta itemProp="title" content="Servant Leadership Essentials Training Program | Blanchard India" />
+                <meta itemProp="description" content="Join Servant Leadership Essentials Training, hands-on webinars, workshops, leadership books and more. Enquire to Discover how to inspire your team today!" />
+                <meta itemProp="image" content="https://byldgroup.com/kbassets/img/Blanchard_Global%20Partner%20Logo_India-01.png" />
+
+                {/* script schema */}
+
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            "@context": "https://schema.org/",
+                            "@type": "WebSite",
+                            "name": "Blanchard India",
+                            "url": "https://byldgroup.com/blanchardindia/",
+                            "potentialAction": {
+                                "@type": "SearchAction",
+                                "target": "https://byldgroup.com/blanchardindia/our-content/programs/servant-leadership-essentials{search_term_string}",
+                                "query-input": "required name=search_term_string"
+                            }
+                        }),
+                    }}
+                />
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            "@context": "https://schema.org",
+                            "@type": "Organization",
+                            "name": "Blanchard Research and Training India",
+                            "alternateName": "Blanchard India",
+                            "url": "https://byldgroup.com/blanchardindia",
+                            "logo": "https://byldgroup.com/kbassets/img/Blanchard_Global%20Partner%20Logo_India-01.png",
+                            "contactPoint": {
+                                "@type": "ContactPoint",
+                                "telephone": "18001021345",
+                                "contactType": "customer service",
+                                "contactOption": "TollFree",
+                                "areaServed": "IN",
+                                "availableLanguage": "en"
+                            },
+                            "sameAs": [
+                                "https://www.facebook.com/blanchardresearchandtrainingindia",
+                                "https://twitter.com/blanchard_india",
+                                "https://www.instagram.com/blanchard_research_india/",
+                                "https://www.youtube.com/c/BlanchardinternationalIndia",
+                                "https://www.linkedin.com/company/blanchard-research-and-training-india/",
+                                "https://byldgroup.com/blanchardindia"
+                            ]
+                        }),
+                    }}
+                />
+
             </Head>
 
             <section className="bredkums2 colortheme4bg pbb-100 border-bottom-left-radius-70">
@@ -40,13 +109,13 @@ export default function ThanYouLP() {
                     <div className="row align-items-center">
                         <div className="col-sm-6">
                             <h2 className="sectionh">
-                            Building the next generation of servant leaders
+                                Building the next generation of servant leaders
                             </h2>
                             <p className="headingp">
-                            The best work relationships are partnerships. They require collaboration between the leader and their people regarding communication, working style, feedback, direction, and support. However, many leaders lose sight of their people’s needs and aspirations when they are focused on meeting the needs of their organization.
+                                The best work relationships are partnerships. They require collaboration between the leader and their people regarding communication, working style, feedback, direction, and support. However, many leaders lose sight of their people’s needs and aspirations when they are focused on meeting the needs of their organization.
                             </p>
                             <p className="headingp">
-                            Blanchard's Servant Leadership Essentials™ course teaches leaders how to build trust, listen, and give and receive feedback in a way that motivates and engages their people.
+                                Blanchard's Servant Leadership Essentials™ course teaches leaders how to build trust, listen, and give and receive feedback in a way that motivates and engages their people.
                             </p>
 
                             <a href='/blanchardindia/get-started' className='kbbtn kbbtn--orange mtt-20'>Talk to an Expert</a>
@@ -72,7 +141,7 @@ export default function ThanYouLP() {
                         <div className="col-sm-6">
                             <h2 className="sectionh">A leadership style that inspires today's workforce to achieve their full potential</h2>
                             <p className="headingp text-justify">
-                            Servant leadership is an optimal way to lead, as it encourages higher levels of engagement and performance and bolsters people's fulfillment. Blanchard's Servant Leadership Essentials impart the skills leaders need to take charge of the process while cultivating a meaningful partnership with their team. This approach instills accountability in the performance of others. Leaders further learn how to direct activities, inject enthusiasm, and look toward the future.
+                                Servant leadership is an optimal way to lead, as it encourages higher levels of engagement and performance and bolsters people's fulfillment. Blanchard's Servant Leadership Essentials impart the skills leaders need to take charge of the process while cultivating a meaningful partnership with their team. This approach instills accountability in the performance of others. Leaders further learn how to direct activities, inject enthusiasm, and look toward the future.
                             </p>
                         </div>
 
@@ -91,7 +160,7 @@ export default function ThanYouLP() {
                         <div className="col-sm-10 text-center pbb-40">
                             <h2>Delivery Options to Meet Your Needs</h2>
                             <p className="pcl">
-                            Servant Leadership can be delivered as virtual sessions and as part of a learning journey so your leaders can learn how to think differently about leadership’s purpose and bring out the best in their people.
+                                Servant Leadership can be delivered as virtual sessions and as part of a learning journey so your leaders can learn how to think differently about leadership’s purpose and bring out the best in their people.
                             </p>
                         </div>
 
@@ -99,7 +168,7 @@ export default function ThanYouLP() {
                             <div className="ourstabs">
                                 <Tabs>
                                     <TabList className="lefttab">
-                                      
+
                                         <Tab>
                                             <img className="" src="/kbassets/img/kbnew/icon/Virtual.svg" alt="Images" />
                                             <span>Virtual</span>
@@ -111,13 +180,13 @@ export default function ThanYouLP() {
                                             <div className="tabinner">
                                                 <h3>Virtual Instructor-led Course</h3>
                                                 <p>
-                                                Blanchard’s Servant Leadership Essentials is delivered through two 2-hour live-virtual sessions and can be built into learning journeys.
+                                                    Blanchard’s Servant Leadership Essentials is delivered through two 2-hour live-virtual sessions and can be built into learning journeys.
                                                 </p>
-                                               <ul>
-                                                <li>This course can be delivered on its own or included in learning journeys.</li>
-                                                <li>Leaders will learn how to build trust, give and receive feedback, and listen to learn.</li>
-                                                <li>Participants receive a set of micro-activities, including videos, interactions, and worksheets, that they can access in moments of need to support ongoing learning, performance support, reinforcement, and custom learning journeys.</li>
-                                               </ul>
+                                                <ul>
+                                                    <li>This course can be delivered on its own or included in learning journeys.</li>
+                                                    <li>Leaders will learn how to build trust, give and receive feedback, and listen to learn.</li>
+                                                    <li>Participants receive a set of micro-activities, including videos, interactions, and worksheets, that they can access in moments of need to support ongoing learning, performance support, reinforcement, and custom learning journeys.</li>
+                                                </ul>
                                             </div>
                                         </div>
                                     </TabPanel>
@@ -140,23 +209,23 @@ export default function ThanYouLP() {
                         <div className='col-sm-6'>
                             <div>
                                 <h2>
-                                Your people and your business-will thrive under servant leadership
+                                    Your people and your business-will thrive under servant leadership
                                 </h2>
                                 <p className="themepbold pbb-10">
-                                Employees quit for a variety of reasons.
+                                    Employees quit for a variety of reasons.
                                 </p>
 
                                 <p className="themepbold pbb-10">
-                                It is estimated that <b><span>54%</span></b> of employees do not feel valued by their employers.
+                                    It is estimated that <b><span>54%</span></b> of employees do not feel valued by their employers.
                                 </p>
                                 <p className="themepbold pbb-10">
-                                Managers don't feel valued by <b><span>52%</span></b> of employees.
+                                    Managers don't feel valued by <b><span>52%</span></b> of employees.
                                 </p>
                                 <p className="themepbold pbb-10">
-                                In the workplace, <b><span>51%</span></b> of employees don't feel like they belong.
+                                    In the workplace, <b><span>51%</span></b> of employees don't feel like they belong.
                                 </p>
-                                
-                                
+
+
 
                             </div>
                         </div>
@@ -177,10 +246,10 @@ export default function ThanYouLP() {
                         <div className='col-sm-8 pbb-70'>
                             <div className="text-center">
                                 <h2 className="sectionh">
-                                Develop a servant leadership mindset and skillset
+                                    Develop a servant leadership mindset and skillset
                                 </h2>
                                 <p>
-                                The most effective leaders recognize that leadership is not about them and that the success of their people fuels their success. Servant leaders continually try to find out what their people need to succeed. They want to make a difference in the lives of their people and, in the process, benefit the organization. Team members perform better when they believe their leader has their best interests at heart.
+                                    The most effective leaders recognize that leadership is not about them and that the success of their people fuels their success. Servant leaders continually try to find out what their people need to succeed. They want to make a difference in the lives of their people and, in the process, benefit the organization. Team members perform better when they believe their leader has their best interests at heart.
                                 </p>
                             </div>
                         </div>
@@ -191,7 +260,7 @@ export default function ThanYouLP() {
                                 <img className="" src="/kbassets/img/kbnew/icon/Accelerate-Learning-Development.svg" alt="Images" />
                                 <h3 className="lineh34">Enhance learning and development</h3>
                                 <p>
-                                Leaders who serve their team members learn how to support and help them grow.
+                                    Leaders who serve their team members learn how to support and help them grow.
                                 </p>
                             </div>
                         </div>
@@ -201,7 +270,7 @@ export default function ThanYouLP() {
                                 <img className="" src="/kbassets/img/kbnew/icon/boost-engagement-and-employee-referrals.svg" alt="Images" />
                                 <h3 className="lineh34">Increase employee engagement and referrals</h3>
                                 <p>
-                                As a result, employees are more likely to stay and endorse the company as a great employer.
+                                    As a result, employees are more likely to stay and endorse the company as a great employer.
                                 </p>
                             </div>
                         </div>
@@ -211,7 +280,7 @@ export default function ThanYouLP() {
                                 <img className="" src="/kbassets/img/kbnew/icon/improve-collaboration.svg" alt="Images" />
                                 <h3 className="lineh34">Improve Collaboration</h3>
                                 <p>
-                                To achieve the extraordinary, servant leaders support, motivate, and empower their team members.
+                                    To achieve the extraordinary, servant leaders support, motivate, and empower their team members.
                                 </p>
                             </div>
                         </div>
@@ -220,7 +289,7 @@ export default function ThanYouLP() {
                                 <img className="" src="/kbassets/img/kbnew/icon/increase-goal-attainment-and-innovation.svg" alt="Images" />
                                 <h3 className="lineh34">Improve goal attainment and innovation</h3>
                                 <p>
-                                People can develop innovative solutions to challenges with the help of servant leaders.
+                                    People can develop innovative solutions to challenges with the help of servant leaders.
                                 </p>
                             </div>
                         </div>
@@ -237,7 +306,7 @@ export default function ThanYouLP() {
                         <div className="col-sm-6 text-center">
                             <h2 className="sectionh">Are you ready to get started?</h2>
                             <p className="headingp pbb-10">
-                            Learn how we can help your leaders become more effective communicators by contacting us today.
+                                Learn how we can help your leaders become more effective communicators by contacting us today.
                             </p>
                             <a href='/blanchardindia/get-started' className='kbbtn kbbtn--orange  mtt-10'>Talk to an Expert</a>
                         </div>
