ba41b00fbd8aeaffea266e3b0b036760e8e692d6
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-29 09:59:45 +0530

    Update creating-psychological-safety-with-randy-conley.js
    update canonical

--- pages/blanchardindia/podcast/creating-psychological-safety-with-randy-conley.js
+++ pages/blanchardindia/podcast/creating-psychological-safety-with-randy-conley.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - Creating Psychological Safety with Randy Conley</title>
+        <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/building-high-performance-habits-with-brendon-burchard" />
         <meta name="description" content="Hear Christina Maslach talk about how to recognize the signs of employee burnout and how to combat the issue to promote increased productivity and health."/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
