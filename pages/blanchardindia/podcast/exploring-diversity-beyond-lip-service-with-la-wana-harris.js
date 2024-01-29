833cdb3186989cf4ba2da7d2f56ddf0537cad296
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-29 10:19:30 +0530

    Update exploring-diversity-beyond-lip-service-with-la-wana-harris.js
    update canonical

--- pages/blanchardindia/podcast/exploring-diversity-beyond-lip-service-with-la-wana-harris.js
+++ pages/blanchardindia/podcast/exploring-diversity-beyond-lip-service-with-la-wana-harris.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - Exploring Diversity Beyond Lip Service with La’Wana Harris</title>
+        <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/exploring-diversity-beyond-lip-service-with-la-wana-harris" />
         <meta name="description" content="Hear Christina Maslach talk about how to recognize the signs of employee burnout and how to combat the issue to promote increased productivity and health."/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
