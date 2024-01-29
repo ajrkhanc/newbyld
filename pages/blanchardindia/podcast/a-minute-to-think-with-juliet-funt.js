b43d52f9d1d4b7e3a7cf212416bb73c8aac353ba
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-29 09:41:09 +0530

    Update a-minute-to-think-with-juliet-funt.js
    update canonical

--- pages/blanchardindia/podcast/a-minute-to-think-with-juliet-funt.js
+++ pages/blanchardindia/podcast/a-minute-to-think-with-juliet-funt.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - A Minute to Think with Juliet Funt</title>
+        <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/a-minute-to-think-with-juliet-funt" />
         <meta name="description" content="Hear Christina Maslach talk about how to recognize the signs of employee burnout and how to combat the issue to promote increased productivity and health."/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
