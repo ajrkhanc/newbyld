c932a04b7cf03eeeddcd36dc1bc1ba424509bd5d
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-29 10:15:26 +0530

    Update simple-truths-of-leadership-with-ken-blanchard-and-randy-conley.js
    update canonical

--- pages/blanchardindia/podcast/simple-truths-of-leadership-with-ken-blanchard-and-randy-conley.js
+++ pages/blanchardindia/podcast/simple-truths-of-leadership-with-ken-blanchard-and-randy-conley.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - Simple Truths of Leadership with Ken Blanchard and Randy Conley</title>
+        <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/simple-truths-of-leadership-with-ken-blanchard-and-randy-conley" />
         <meta name="description" content="Hear Christina Maslach talk about how to recognize the signs of employee burnout and how to combat the issue to promote increased productivity and health."/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
