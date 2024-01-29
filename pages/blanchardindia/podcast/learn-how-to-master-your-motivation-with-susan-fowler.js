2fcf5ca3792c816206cc81a03c66d0d432d4b820
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-29 10:13:25 +0530

    Update learn-how-to-master-your-motivation-with-susan-fowler.js
    update canonical

--- pages/blanchardindia/podcast/learn-how-to-master-your-motivation-with-susan-fowler.js
+++ pages/blanchardindia/podcast/learn-how-to-master-your-motivation-with-susan-fowler.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - Learn How to Master Your Motivation with Susan Fowler</title>
+        <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/learn-how-to-master-your-motivation-with-susan-fowler" />
         <meta name="description" content="Hear Christina Maslach talk about how to recognize the signs of employee burnout and how to combat the issue to promote increased productivity and health."/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
