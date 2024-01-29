d4c4eb564a08a353068d27bf85b6898f79305675
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-01-29 09:47:14 +0530

    Update the-awesome-human-project-with-nataly-kogan.js
    update canonical

--- pages/blanchardindia/podcast/the-awesome-human-project-with-nataly-kogan.js
+++ pages/blanchardindia/podcast/the-awesome-human-project-with-nataly-kogan.js
@@ -6,6 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - Best of 2022: The Awesome Human Project with Nataly Kogan</title>
+        <link rel="canonical" href="https://byldgroup.com/blanchardindia/podcast/the-awesome-human-project-with-nataly-kogan" />
         <meta name="description" content="Hear Christina Maslach talk about how to recognize the signs of employee burnout and how to combat the issue to promote increased productivity and health."/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
