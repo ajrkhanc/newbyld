21df61959a29a6d6770a6dbe05ff2f6fb6f71410
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-02-01 11:13:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+
+
+
 module.exports = {
   reactStrictMode: true,
 
@@ -20,6 +35,10 @@ module.exports = {
           {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
           },
         ],
       },
