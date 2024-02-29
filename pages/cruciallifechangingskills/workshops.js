f7c562cbfe79dc2737019a9ec726689c6916eee4
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-02-29 15:20:44 +0530

    Update workshops.js

--- pages/cruciallifechangingskills/workshops.js
+++ pages/cruciallifechangingskills/workshops.js
@@ -419,21 +419,7 @@ export default function Workshops(){
                             </div>
                        </div>
                     </div>
-                    <div class="col-md-4 workshop-col"> 
-                       <div class="row workshop-row"> 
-                            <div class="col-md-12 workshop-content gtdcolor"> 
-                            <img class="left-image img100px mbb-30" src="/classets/img/logo-gtd.svg" alt="" />
-                            <h4 className='pbb-20'>Trapologist At work</h4>                            
-                            </div>
-                            <h5 className='eventt'>Workshop</h5>
-                             <div class="col-md-12 workshop-date"> 
-                            <h6><i class="fa fa-calendar" aria-hidden="true"></i> March 18th , 2024</h6>
-                            <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 AM to 5:00 PM</h6>
-                            <a href='#registered'><button class="register gtdcolor">REGISTER NOW</button></a>
-                            </div>
-                       </div>
-                    </div>
-
+                  
                    
                     {/* <div class="col-md-4 workshop-col"> 
                        <div class="row workshop-row"> 
@@ -481,6 +467,21 @@ export default function Workshops(){
                             </div>
                        </div>
                     </div>
+                    <div class="col-md-4 workshop-col"> 
+                       <div class="row workshop-row"> 
+                            <div class="col-md-12 workshop-content gtdcolor"> 
+                            <img class="left-image img100px mbb-30" src="/classets/img/logo-gtd.svg" alt="" />
+                            <h4 className='pbb-20'>Trapologist At work</h4>                            
+                            </div>
+                            <h5 className='eventt'>Workshop</h5>
+                             <div class="col-md-12 workshop-date"> 
+                            <h6><i class="fa fa-calendar" aria-hidden="true"></i> March 18th , 2024</h6>
+                            <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 10:00 AM to 5:00 PM</h6>
+                            <a href='#registered'><button class="register gtdcolor">REGISTER NOW</button></a>
+                            </div>
+                       </div>
+                    </div>
+
 
 
 
