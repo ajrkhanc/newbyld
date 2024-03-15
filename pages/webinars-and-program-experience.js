2a8923090b67860ec7904f51018be549489a909d
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-03-15 15:33:50 +0530

    Update webinars-and-program-experience.js

--- pages/webinars-and-program-experience.js
+++ pages/webinars-and-program-experience.js
@@ -43,7 +43,6 @@ export default function WebinarsAndProgramExperience() {
         <>
             <Head>
                 <title>Webinars and Program Experience - BYLD Group</title>
-                <link rel="canonical" href="https://byldgroup.com/webinars-and-program-experience" />
                 <meta name="description" content="Batches till 20th April 2022 Crucial Life Changing Skills Batches till 20th April 2022 Reserve Your Seat Batches till 29th April 2022 Blanchard India Webinars Batches till 29th April 2022 Reserve Your Seat May 4th, 2022Trapologist at WorkTM Webinar 4th May 2022 Reserve Your Seat Webinar May 6th, 2022Five Behaviours Team Development Webinar 6th May… Continue reading Webinars and Program Experience" />
                 <link rel="stylesheet" type="text/css" href="/assets/css/homemodule.css" />
             </Head>
@@ -80,7 +79,7 @@ export default function WebinarsAndProgramExperience() {
             <section className='ptt-60 pbb-60 positionrelative'>
                 <div className='container'>
                     <div className='row zindx'>
-                        
+
 
 
                         {/* <div className='col-sm-4'>
@@ -97,7 +96,7 @@ export default function WebinarsAndProgramExperience() {
                             </div>
                         </div> */}
 
-                        <div className='col-sm-4'>
+                        {/* <div className='col-sm-4'>
                         <div className='eventsbox'>
                             <div className='eventmiddle'>
                                 <img src="/assets/img/events.jpg" alt="" />
@@ -109,7 +108,7 @@ export default function WebinarsAndProgramExperience() {
                                 <a className="eventbtn" href='#Register'>Reserve Your Seat</a>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
                         {/* <div className='col-sm-4'>
                         <div className='eventsbox'>
@@ -124,7 +123,7 @@ export default function WebinarsAndProgramExperience() {
                             </div>
                         </div>
                     </div> */}
-
+                        {/* 
                     <div className='col-sm-4'>
                         <div className='eventsbox'>
                             <div className='eventmiddle'>
@@ -137,10 +136,10 @@ export default function WebinarsAndProgramExperience() {
                                 <a className="eventbtn" href='#Register'>Reserve Your Seat</a>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
 
-                    {/* <div className='col-sm-4'>
+                        {/* <div className='col-sm-4'>
                         <div className='eventsbox'>
                             <div className='eventmiddle'>
                                 <img src="/assets/img/events.jpg" alt="" />
@@ -154,7 +153,7 @@ export default function WebinarsAndProgramExperience() {
                         </div>
                     </div> */}
 
-                    <div className='col-sm-4'>
+                        {/* <div className='col-sm-4'>
                             <div className='eventsbox'>
                                 <div className='eventmiddle'>
                                     <img src="/assets/img/events.jpg" alt="" />
@@ -166,9 +165,9 @@ export default function WebinarsAndProgramExperience() {
                                     <a className="eventbtn" href='#Register'>Reserve Your Seat</a>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
 
-{/* 
+                        {/* 
                     <div className='col-sm-4'>
                         <div className='eventsbox'>
                             <div className='eventmiddle'>
@@ -219,7 +218,7 @@ export default function WebinarsAndProgramExperience() {
 
 
 
-                        <div className='col-sm-4'>
+                        {/* <div className='col-sm-4'>
                         <div className='eventsbox'>
                             <div className='eventmiddle'>
                                 <img src="/assets/img/events.jpg" alt="" />
@@ -231,12 +230,12 @@ export default function WebinarsAndProgramExperience() {
                                 <a className="eventbtn" href='#Register'>Reserve Your Seat</a>
                             </div>
                         </div>
-                    </div>
-
-                        
+                    </div> */}
 
 
-                    <div className='col-sm-4'>
+
+
+                        <div className='col-sm-4'>
                             <div className='eventsbox'>
                                 <div className='eventmiddle'>
                                     <img src="/assets/img/events.jpg" alt="" />
@@ -250,9 +249,9 @@ export default function WebinarsAndProgramExperience() {
                             </div>
                         </div>
 
-                        
 
-                        
+
+
 
                         {/* <div className='col-sm-4'>
                             <div className='eventsbox'>
@@ -268,7 +267,7 @@ export default function WebinarsAndProgramExperience() {
                             </div>
                         </div> */}
 
-                      
+
                         {/* <div className='col-sm-4'>
                             <div className='eventsbox'>
                                 <div className='eventmiddle'>
@@ -311,7 +310,7 @@ export default function WebinarsAndProgramExperience() {
                             </div>
                         </div> */}
 
-                        
+
 
                         {/* <div className='col-sm-4'>
                             <div className='eventsbox'>
@@ -385,84 +384,84 @@ export default function WebinarsAndProgramExperience() {
             <div id="Register" class="homecon ptt-60">
                 <div className="container">
 
-                <div className='row'>
+                    <div className='row'>
                         <div className='col-sm-12 text-center'>
                             <h2 class="title mb-0 md-pb-20 h2call wow fadeInUp delay-0-2s animated animateUP">Register Now</h2>
                         </div>
                     </div>
-                    <div class="row y-middle">                      
+                    <div class="row y-middle">
 
-                    <div class="col-lg-5 md-mb-50">
+                        <div class="col-lg-5 md-mb-50">
                             <div class="contact-img wow fadeInUp delay-0-2s animated animateUP">
                                 <img src="/assets/img/homeb/contactl.png" alt="Contact" />
                             </div>
                         </div>
 
                         <div class="col-lg-7">
-                        <div class="contact-wrap">
-                            <div className="bannerform wow fadeInUp delay-0-2s animated animateUP">
-                                <form id="contact-form" className='clientcornner ptt-5 pbb-20' onSubmit={registerUser}>
-                                    <div className="row">
-              
-                                        <div className="col-sm-6 mb-12">
-                                            <input type="text" className='borrr' name="name" placeholder="Your Name*" required />
-                                        </div>
-                                        <div className="col-sm-6 mb-12">
-                                            <input type="email" className='borrr' name="email" placeholder="Work Email*" required />
-                                        </div>
-                                        <div className="col-sm-6 mb-12">
-                                            <input type="text" className='borrr' name="phone" maxlength="10" minlength="10" pattern="[0-9]*" placeholder="Phone No.*" required />
-                                        </div>
-                                        <div className="col-sm-6 mb-12">
-                                            <input type="text" className='borrr' name="organization" placeholder="Organization*" required />
-                                        </div>
-                                        <div className="col-sm-6 mb-12">
-                                            <input type="text" className='borrr' name="Designation" placeholder="Designation*" required />
-                                        </div>
-                                        <div className="col-sm-6 mb-12">
-                                            <input type="text" className='borrr' name="Location" placeholder="Location*" required />
-                                        </div>
-                                        <div className="col-sm-6 mb-12">
-                                            <select name="leadsquared_mx_Showed_Interest_in" required>
-                                                <option value="">Pick up any Slot*</option>
-                                                {/* <option value="Everything DiSC Work Productive Conflict, 5th July, 2023">Everything DiSC Work Productive Conflict, 5th July, 2023</option>                                               
-                                                <option value="Everything DiSC Agile EQ, 19th July, 2023">Everything DiSC Agile EQ, 19th July, 2023</option> */}
-                                                {/* <option value="Five Behaviour Personal Development, 2nd August, 2023">Five Behaviour Personal Development, 2nd August, 2023</option> */}
-                                                {/* <option value="Inside Out Coaching, 23rd August 2023">Inside Out Coaching, 23rd August 2023</option>
-                                                <option value="Lumina Spark, 23rd August, 2023">Lumina Spark, 23rd August, 2023</option> */}
-                                                <option value="Getting Things Done® – January 5th, 2024">Getting Things Done® – January 5th, 2024</option>
-                                                <option value="Crucial Influence® – January 5th, 2024">Crucial Influence® – January 5th, 2024</option>                                                
-                                                <option value="Crucial Conversations for Accountability® – January 30th, 2024">Crucial Conversations for Accountability® – January 30th, 2024</option>
-                                                <option value="Crucial Influence® – February 20th, 2024">Crucial Influence® – February 20th, 2024</option>
-                                                <option value="Crucial Conversations® For Mastering Dialogue – March 8th, 2024">Crucial Conversations® For Mastering Dialogue – March 8th, 2024</option>
-                                                <option value="Getting Things Done® – March 14th, 2024">Getting Things Done® – March 14th, 2024</option>
-                                                <option value="Trapologist at Work® – February 13th, 2024">Trapologist at Work® – February 13th, 2024</option>
+                            <div class="contact-wrap">
+                                <div className="bannerform wow fadeInUp delay-0-2s animated animateUP">
+                                    <form id="contact-form" className='clientcornner ptt-5 pbb-20' onSubmit={registerUser}>
+                                        <div className="row">
 
-                                            </select>
+                                            <div className="col-sm-6 mb-12">
+                                                <input type="text" className='borrr' name="name" placeholder="Your Name*" required />
+                                            </div>
+                                            <div className="col-sm-6 mb-12">
+                                                <input type="email" className='borrr' name="email" placeholder="Work Email*" required />
+                                            </div>
+                                            <div className="col-sm-6 mb-12">
+                                                <input type="text" className='borrr' name="phone" maxlength="10" minlength="10" pattern="[0-9]*" placeholder="Phone No.*" required />
+                                            </div>
+                                            <div className="col-sm-6 mb-12">
+                                                <input type="text" className='borrr' name="organization" placeholder="Organization*" required />
+                                            </div>
+                                            <div className="col-sm-6 mb-12">
+                                                <input type="text" className='borrr' name="Designation" placeholder="Designation*" required />
+                                            </div>
+                                            <div className="col-sm-6 mb-12">
+                                                <input type="text" className='borrr' name="Location" placeholder="Location*" required />
+                                            </div>
+                                            <div className="col-sm-6 mb-12">
+                                                <select name="leadsquared_mx_Showed_Interest_in" required>
+                                                    <option value="">Pick up any Slot*</option>
+                                                    {/* <option value="Everything DiSC Work Productive Conflict, 5th July, 2023">Everything DiSC Work Productive Conflict, 5th July, 2023</option>                                               
+                                                <option value="Everything DiSC Agile EQ, 19th July, 2023">Everything DiSC Agile EQ, 19th July, 2023</option> */}
+                                                    {/* <option value="Five Behaviour Personal Development, 2nd August, 2023">Five Behaviour Personal Development, 2nd August, 2023</option> */}
+                                                    {/* <option value="Inside Out Coaching, 23rd August 2023">Inside Out Coaching, 23rd August 2023</option>
+                                                <option value="Lumina Spark, 23rd August, 2023">Lumina Spark, 23rd August, 2023</option> */}
+                                                    <option value="Getting Things Done® – January 5th, 2024">Getting Things Done® – January 5th, 2024</option>
+                                                    <option value="Crucial Influence® – January 5th, 2024">Crucial Influence® – January 5th, 2024</option>
+                                                    <option value="Crucial Conversations for Accountability® – January 30th, 2024">Crucial Conversations for Accountability® – January 30th, 2024</option>
+                                                    <option value="Crucial Influence® – February 20th, 2024">Crucial Influence® – February 20th, 2024</option>
+                                                    <option value="Crucial Conversations® For Mastering Dialogue – March 8th, 2024">Crucial Conversations® For Mastering Dialogue – March 8th, 2024</option>
+                                                    <option value="Getting Things Done® – March 14th, 2024">Getting Things Done® – March 14th, 2024</option>
+                                                    <option value="Trapologist at Work® – February 13th, 2024">Trapologist at Work® – February 13th, 2024</option>
+
+                                                </select>
+                                            </div>
+                                            <div className="col-sm-6 mb-12">
+                                                <select name="referredby" required>
+                                                    <option value="">Referred By*</option>
+                                                    <option value="Social Media">Social Media</option>
+                                                    <option value="Google Search">Google Search</option>
+                                                    <option value="Reference">Reference</option>
+                                                </select>
+                                            </div>
+                                            <div className="col-sm-6 mb-12 d-none">
+                                                <select name="leadsquared_mx_Business_Entity" required>
+                                                    <option value="BYLD Group">BYLD Group</option>
+                                                </select>
+                                            </div>
+                                            <div className="col-lg-12 mb-12">
+                                                <textarea className="from-control" name="QuestionsComments" placeholder="Questions/Comments:"></textarea>
+                                            </div>
+                                            <div className="col-lg-12 mb-12">
+                                                <input id="submitbuttonform" className="clientcornnerbtn width150" type="submit" value="Get in Touch" />
+                                            </div>
+                                            <p id="showlabel" style={{ display: "none" }}></p>
                                         </div>
-                                        <div className="col-sm-6 mb-12">
-                                            <select name="referredby" required>
-                                                <option value="">Referred By*</option>
-                                                <option value="Social Media">Social Media</option>
-                                                <option value="Google Search">Google Search</option>
-                                                <option value="Reference">Reference</option>
-                                            </select>
-                                        </div>
-                                        <div className="col-sm-6 mb-12 d-none">
-                                            <select name="leadsquared_mx_Business_Entity" required>
-                                                <option value="BYLD Group">BYLD Group</option>
-                                            </select>
-                                        </div>
-                                        <div className="col-lg-12 mb-12">
-                                            <textarea className="from-control" name="QuestionsComments" placeholder="Questions/Comments:"></textarea>
-                                        </div>
-                                        <div className="col-lg-12 mb-12">
-                                            <input id="submitbuttonform" className="clientcornnerbtn width150" type="submit" value="Get in Touch" />
-                                        </div>
-                                        <p id="showlabel" style={{ display: "none" }}></p>
-                                    </div>
-                                </form>
-                            </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
 
