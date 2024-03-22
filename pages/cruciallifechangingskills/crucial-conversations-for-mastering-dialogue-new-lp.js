637f0a6e1759fc1c0e5080b1d233cc358603bb5e
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-03-22 15:00:30 +0530

    Update crucial-conversations-for-mastering-dialogue-new-lp.js

--- pages/cruciallifechangingskills/crucial-conversations-for-mastering-dialogue-new-lp.js
+++ pages/cruciallifechangingskills/crucial-conversations-for-mastering-dialogue-new-lp.js
@@ -429,7 +429,7 @@ export default function BrowseCourses() {
                 </div>
               </div>
             </div> */}
-                       <div class="col-md-4 workshop-col">
+            <div class="col-md-4 workshop-col">
               <div class="row workshop-row">
                 <div class="col-md-12 workshop-content">
                   <img
@@ -449,6 +449,31 @@ export default function BrowseCourses() {
                   <h6>
                     <i class="fa fa-clock-o" aria-hidden="true"></i> 3:00 PM to
                     4:00 PM
+                  </h6>
+                  <a href="#registered">
+                    <button class="register">REGISTER NOW</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 workshop-col">
+              <div class="row workshop-row">
+                <div class="col-md-12 workshop-content">
+                  <img
+                    class="left-image img100px mbb-30"
+                    src="/classets/img/logo-influencer.svg"
+                    alt=""
+                  />
+                  <h4>CRUCIAL CONVERSATIONS</h4>
+                  <p>for Mastering Dialogue</p>
+                </div>
+                <h5 className="eventt"> Workshop</h5>
+                <div class="col-md-12 workshop-date">
+                  <h6>
+                    <i class="fa fa-calendar" aria-hidden="true"></i>18th & 19th April
+                  </h6>
+                  <h6>
+                    <i class="fa fa-clock-o" aria-hidden="true"></i> 9:00 AM - 5:30 PM
                   </h6>
                   <a href="#registered">
                     <button class="register">REGISTER NOW</button>
