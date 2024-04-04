121dfae4e829357497b300b9d96c0c2f7b8e1246
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-04 13:31:02 +0530

    Update join-our-partner-network-lp-ab-testing.js

--- pages/join-our-partner-network-lp-ab-testing.js
+++ pages/join-our-partner-network-lp-ab-testing.js
@@ -9,7 +9,7 @@ export default function JoinOurPartnerNetworkLP(){
         xhttp.onload = function () {
             console.log(this.responseText);
         }
-        xhttp.open("Post", 'https://ajrkhan.xyz/byldgroup/wp-json/contact-form-7/v1/contact-forms/13/feedback');
+        xhttp.open("Post", 'https://byldgroup.in/byldgroup/wp-json/contact-form-7/v1/contact-forms/13/feedback');
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4) {
