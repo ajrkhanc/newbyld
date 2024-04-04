fab491b62611d97de7fe671fa9132038cb8a9528
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-04 11:59:39 +0530

    Update everything-disc-ttt-vt-lp.js

--- pages/everythingdisc/everything-disc-ttt-vt-lp.js
+++ pages/everythingdisc/everything-disc-ttt-vt-lp.js
@@ -10,7 +10,7 @@ export default function DiskTTTLP(){
         xhttp.onload = function () {
             console.log(this.responseText);
         }
-        xhttp.open("Post", 'https://ajrkhan.xyz/everythingdisc/wp-json/contact-form-7/v1/contact-forms/17/feedback');
+        xhttp.open("Post", 'https://byldgroup.in/everythingdisc/wp-json/contact-form-7/v1/contact-forms/17/feedback');
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4) {
