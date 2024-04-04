e29830dbaa446900e3fb27239b4368c689215d6d
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-04 12:32:50 +0530

    Update ed-sales-workshop.js

--- pages/everythingdisc/ed-sales-workshop.js
+++ pages/everythingdisc/ed-sales-workshop.js
@@ -9,7 +9,7 @@ export default function EDSalesWorkshop(){
         xhttp.onload = function () {
             console.log(this.responseText);
         }
-        xhttp.open("Post", 'https://ajrkhan.xyz/everythingdisc/wp-json/contact-form-7/v1/contact-forms/72/feedback');
+        xhttp.open("Post", 'https://byldgroup.in/everythingdisc/wp-json/contact-form-7/v1/contact-forms/72/feedback');
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4) {
