058df86d9433b04125c45b1a7ee3c3a2fcd58a5d
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-04 15:17:38 +0530

    Update conversational-capacity-webinar.js

--- pages/blanchardindia/lp/conversational-capacity-webinar.js
+++ pages/blanchardindia/lp/conversational-capacity-webinar.js
@@ -11,7 +11,7 @@ export default function ConversationalCapacityWebinar() {
       xhttp.onload = function () {
          // console.log(this.responseText.status);
       }
-      xhttp.open("Post", 'https://ajrkhan.xyz/blanchardinternational/wp-json/contact-form-7/v1/contact-forms/297/feedback');
+      xhttp.open("Post", 'https://byldgroup.in/blanchardinternational/wp-json/contact-form-7/v1/contact-forms/297/feedback');
       xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
       xhttp.onreadystatechange = function () {
          if (xhttp.readyState == 4) {
