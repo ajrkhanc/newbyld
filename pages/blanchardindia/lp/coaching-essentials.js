17b51f114adb25331b8f04766a01937893448088
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-05 14:37:32 +0530

    Update coaching-essentials.js

--- pages/blanchardindia/lp/coaching-essentials.js
+++ pages/blanchardindia/lp/coaching-essentials.js
@@ -11,7 +11,7 @@ export default function HybridWorkplaces() {
       xhttp.onload = function () {
          // console.log(this.responseText.status);
       }
-      xhttp.open("Post", 'https://bmelectrician.com/blanchard/wp-json/contact-form-7/v1/contact-forms/30378/feedback');
+      xhttp.open("Post", https://byldgroup.in/blanchardinternational/wp-json/contact-form-7/v1/contact-forms/83/feedback');
       xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
       xhttp.onreadystatechange = function () {
          if (xhttp.readyState == 4) {
