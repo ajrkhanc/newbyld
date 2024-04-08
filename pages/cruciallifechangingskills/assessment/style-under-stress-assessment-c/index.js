6444ff6e16e75dbddddf0645ed339a8c7ecd8a6a
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-08 16:55:08 +0530

    Update index.js

--- pages/cruciallifechangingskills/assessment/style-under-stress-assessment-c/index.js
+++ pages/cruciallifechangingskills/assessment/style-under-stress-assessment-c/index.js
@@ -117,7 +117,7 @@ export default function BrowseCourses() {
             xhttp.onload = function () {
                 console.log(this.responseText);
             }
-            xhttp.open("Post", 'https://ajrkhan.xyz/cruciallifechangingskills/wp-json/contact-form-7/v1/contact-forms/176/feedback');
+            xhttp.open("Post", 'https://byldgroup.in/cruciallifechangingskills/wp-json/contact-form-7/v1/contact-forms/176/feedback');
             xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     
        
