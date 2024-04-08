07bbf4800cf57ef1ce69a491e5d13d0268d6387f
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-08 17:42:14 +0530

    Update contact-us.js

--- pages/cruciallifechangingskills/contact-us.js
+++ pages/cruciallifechangingskills/contact-us.js
@@ -9,7 +9,7 @@ export default function ContactUs(){
         xhttp.onload = function () {
             console.log(this.responseText);
         }
-        xhttp.open("Post", 'https://ajrkhan.xyz/cruciallifechangingskills/wp-json/contact-form-7/v1/contact-forms/6/feedback');
+        xhttp.open("Post", 'https://byldgroup.in/cruciallifechangingskills/wp-json/contact-form-7/v1/contact-forms/6/feedback');
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4) {
