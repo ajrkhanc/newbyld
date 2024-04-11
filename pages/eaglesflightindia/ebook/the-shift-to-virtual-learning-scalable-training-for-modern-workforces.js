a0169281a1e10869809b79b1aea7cb16e06cb248
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-11 16:25:08 +0530

    Update the-shift-to-virtual-learning-scalable-training-for-modern-workforces.js

--- pages/eaglesflightindia/ebook/the-shift-to-virtual-learning-scalable-training-for-modern-workforces.js
+++ pages/eaglesflightindia/ebook/the-shift-to-virtual-learning-scalable-training-for-modern-workforces.js
@@ -9,7 +9,7 @@ export default function Ebookpage(){
         xhttp.onload = function () {
             console.log(this.responseText);
         }
-        xhttp.open("Post", 'https://ajrkhan.xyz/eaglesflightindia/wp-json/contact-form-7/v1/contact-forms/5/feedback');
+        xhttp.open("Post", 'https://byldgroup.in/eaglesflightindia/wp-json/contact-form-7/v1/contact-forms/5/feedback');
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4) {
