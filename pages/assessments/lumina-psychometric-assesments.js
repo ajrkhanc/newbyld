06709e46fbdeaace4abcfcc07b23b0b28dde8163
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-11 18:08:57 +0530

    Update lumina-psychometric-assesments.js

--- pages/assessments/lumina-psychometric-assesments.js
+++ pages/assessments/lumina-psychometric-assesments.js
@@ -11,7 +11,7 @@ export default function LuminaPsychomentricAssesments(){
         xhttp.onload = function () {
             console.log(this.responseText);
         }
-        xhttp.open("Post", 'https://ajrkhan.xyz/byldgroup/wp-json/contact-form-7/v1/contact-forms/55/feedback');
+        xhttp.open("Post", 'https://byldgroup.in/byldgroup/wp-json/contact-form-7/v1/contact-forms/55/feedback');
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4) {
