86e75579ac2d04b901f167b78227caa4872a6c21
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-04-19 16:08:03 +0530

    Update webinars.js
    add new card

--- pages/cruciallifechangingskills/webinars.js
+++ pages/cruciallifechangingskills/webinars.js
@@ -82,7 +82,35 @@ export default function Webinars() {
                                 </div>
                             </div>
                         </div> */}
-
+                        <div class="col-md-4 workshop-col"> 
+                       <div class="row workshop-row"> 
+                            <div class="col-md-12 workshop-content"> 
+                            <img class="left-image img100px mbb-30" src="/classets/img/logo-influencer.svg" alt="" />
+                            <h4>CRUCIAL CONVERSATIONS</h4>
+                            <p>for Accountability</p>
+                            </div>
+                            <h5 className='eventt'>Virtual Course Preview</h5>
+                             <div class="col-md-12 workshop-date"> 
+                            <h6><i class="fa fa-calendar" aria-hidden="true"></i> 16th May, 2024</h6>
+                            <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 3:00 PM to 4:00 PM</h6>
+                            <a href='#registered'><button class="register">REGISTER NOW</button></a>
+                            </div>
+                       </div>
+                    </div> 
+                    <div class="col-md-4 workshop-col"> 
+                       <div class="row workshop-row"> 
+                            <div class="col-md-12 workshop-content gtdcolor"> 
+                            <img class="left-image img100px mbb-30" src="/classets/img/logo-gtd.svg" alt="" />
+                            <h4 className='pbb-20'>Trapologist At work</h4>                            
+                            </div>
+                            <h5 className='eventt'>Virtual Course Preview</h5>
+                             <div class="col-md-12 workshop-date"> 
+                            <h6><i class="fa fa-calendar" aria-hidden="true"></i> 31st May , 2024</h6>
+                            <h6><i class="fa fa-clock-o" aria-hidden="true"></i> 3:00 PM to 4:00 PM</h6>
+                            <a href='#registered'><button class="register gtdcolor">REGISTER NOW</button></a>
+                            </div>
+                       </div>
+                    </div> 
                         <div class="col-md-4 workshop-col">
                             <div class="row workshop-row">
                                 <div class="col-md-12 workshop-content phothemecolor">
