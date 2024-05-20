14af5032b6eb7ee73999e8e841846ef9a7cf4abb
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-05-20 12:09:29 +0530

    Update index.js

--- pages/everythingdisc/index.js
+++ pages/everythingdisc/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       xhttp.onload = function () {
           console.log(this.responseText);
       }
-      xhttp.open("Post", 'https://ajrkhan.xyz/everythingdisc/wp-json/contact-form-7/v1/contact-forms/24/feedback');
+      xhttp.open("Post", 'https://byldgroup.in/everythingdisc/wp-json/contact-form-7/v1/contact-forms/24/feedback');
       xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
       xhttp.onreadystatechange = function () {
           if (xhttp.readyState == 4) {
