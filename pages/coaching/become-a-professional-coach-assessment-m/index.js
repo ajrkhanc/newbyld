3134d7ba8f0094c7c29ac99da4e00f3111126db2
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-05-28 10:42:40 +0530

    Update index.js

--- pages/coaching/become-a-professional-coach-assessment-m/index.js
+++ pages/coaching/become-a-professional-coach-assessment-m/index.js
@@ -59,7 +59,7 @@ export default function BrowseCourses() {
                     xhttp.onload = function () {
                         console.log(this.responseText);
                     }
-                    xhttp.open("Post", 'https://ajrkhan.xyz/byldgroup/wp-json/contact-form-7/v1/contact-forms/53/feedback');
+                    xhttp.open("Post", 'https://byldgroup.in/byldgroup/wp-json/contact-form-7/v1/contact-forms/53/feedback');
                     xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 
@@ -95,7 +95,7 @@ export default function BrowseCourses() {
         // xhttp.onload = function () {
         //     console.log(this.responseText);
         // }
-        // xhttp.open("Post", 'https://ajrkhan.xyz/byldgroup/wp-json/contact-form-7/v1/contact-forms/53/feedback');
+        // xhttp.open("Post", 'https://byldgroup.in/byldgroup/wp-json/contact-form-7/v1/contact-forms/53/feedback');
         // xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 
