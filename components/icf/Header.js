47a87e72eed63af783b230a198b21bebdb0f33eb
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-07-05 15:35:50 +0530

    Update Header.js

--- components/icf/Header.js
+++ components/icf/Header.js
@@ -102,7 +102,7 @@ export default function Header() {
                                                                 <a href="/bespoke">BeSpoke</a>
                                                                 <ul className="sub-menu">
                                                                     <li><a href="/bespoke/persona-global">Persona Global<sup>®</sup></a> </li>
-                                                                    <li><a target='_blank' href="/bespoke/persona-global">Custom Solutions</a></li>
+                                                                    <li><a target='_blank' href="https://doortraining.co.in/">Custom Solutions</a></li>
                                                                     <li className='d-none'><a href="#">SLI</a></li>
                                                                     <li className='d-none'><a href="#">Large Projects</a></li>
                                                                 </ul>
@@ -298,7 +298,7 @@ export default function Header() {
                                         <a href="/bespoke">BeSpoke</a>
                                         <ul className="sub-menu">
                                             <li><a href="/bespoke/persona-global">Persona Global<sup>®</sup></a> </li>
-                                            <li><a target='_blank' href="/bespoke/persona-global">Custom Solutions</a></li>
+                                            <li><a target='_blank' href="https://doortraining.co.in/">Custom Solutions</a></li>
                                             <li className='d-none'><a href="#">SLI</a></li>
                                             <li className='d-none'><a href="#">Large Projects</a></li>
                                         </ul>
