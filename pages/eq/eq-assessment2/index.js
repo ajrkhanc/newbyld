39164dc7a3f0b319c5719825d280c40292255fe8
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-07-23 11:11:02 +0530

    Update index.js

--- pages/eq/eq-assessment2/index.js
+++ pages/eq/eq-assessment2/index.js
@@ -79,7 +79,7 @@ export default function BrowseCourses() {
 
                 if (data.status == 0) {
                     var xhttp = xhr;
-                    xhttp.open("Post", 'https://ajrkhan.xyz/byldgroup/wp-json/contact-form-7/v1/contact-forms/164/feedback');
+                    xhttp.open("Post", 'https://byldgroup.in/byldgroup/wp-json/contact-form-7/v1/contact-forms/164/feedback');
                     xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
                     var Assessment = "EQ+ Assessment 2"
