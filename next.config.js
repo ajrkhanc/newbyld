9ba952c6f5ae1c685751d60a26f3ea23ad5a1af7
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-07-24 16:23:41 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -55,41 +55,7 @@ module.exports = {
         destination: '/404',
         permanent: true,
       },
-       {
-        source: '/coaching/dtci-coaching-academy',
-        destination: '/404',
-        permanent: true,
-      },
-       {
-        source: '/coaching/insideout-coaching',
-        destination: '/404',
-        permanent: true,
-      },
-      {
-        source: '/coaching/executive-coaching',
-        destination: '/404',
-        permanent: true,
-      },
-      {
-        source: '/inside-out-coaching-lp',
-        destination: '/404',
-        permanent: true,
-      },
-       {
-        source: '/coach-certification-lp',
-        destination: '/404',
-        permanent: true,
-      },
-         {
-        source: '/dtci',
-        destination: '/404',
-        permanent: true,
-      },
-        {
-        source: '/who-we-are',
-        destination: '/404',
-        permanent: true,
-      },
+      
       {
         source: '/coaching/become-a-professional-coach-marketing',
         destination: '/coaching/become-a-professional-coach-assessment-m',
