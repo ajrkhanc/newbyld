7edcb4c4ebf63c40b2c80300be671aa4fed584ce
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-07-24 16:14:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -80,6 +80,11 @@ module.exports = {
         destination: '/404',
         permanent: true,
       },
+         {
+        source: '/dtci',
+        destination: '/404',
+        permanent: true,
+      },
       {
         source: '/coaching/become-a-professional-coach-marketing',
         destination: '/coaching/become-a-professional-coach-assessment-m',
