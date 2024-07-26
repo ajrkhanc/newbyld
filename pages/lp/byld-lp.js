ca90bd2b98528482576737ae0086c734a5f485bc
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2024-07-26 12:22:06 +0530

    Update byld-lp.js

--- pages/lp/byld-lp.js
+++ pages/lp/byld-lp.js
@@ -9,7 +9,7 @@ export default function BylDLp(){
         xhttp.onload = function () {
             console.log(this.responseText);
         }
-        xhttp.open("Post", 'https://byldgroup.in/byldgroup/wp-json/contact-form-7/v1/contact-forms/252/feedback');
+        xhttp.open("Post", 'https://byldgroup.in/byldgroup/wp-json/contact-form-7/v1/contact-forms/141/feedback');
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4) {
