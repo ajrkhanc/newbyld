69e3bb31d2875f3868ea781c3974717ba97b833b
Ravi Kumar <155704132+RaviKumarYT@users.noreply.github.com>
2024-08-16 16:49:11 +0530

    Updated code
    Updated code

--- pages/coaching/coaching-snapshot-assessment-m/index.js
+++ pages/coaching/coaching-snapshot-assessment-m/index.js
@@ -100,8 +100,9 @@ export default function BrowseCourses() {
                 document.getElementById("response").innerHTML = data.message;
 
                 if (data.status == 0) {
-                    var xhttp = xhr;                    
-                    xhttp.open("Post", 'https://ajrkhan.xyz/byldgroup/wp-json/contact-form-7/v1/contact-forms/61/feedback');
+                    var xhttp = xhr;  
+                    xhttp.open("Post", 'https://byldgroup.in/byldgroup/wp-json/contact-form-7/v1/contact-forms/61/feedback');                  
+                    //xhttp.open("Post", 'https://ajrkhan.xyz/byldgroup/wp-json/contact-form-7/v1/contact-forms/61/feedback');
                     xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
                     var Assessment = "Coaching - Coaching Snapshot Assessment Marketing"
